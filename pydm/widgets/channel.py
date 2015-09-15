e2f73bef1fca2bcc4351969acf7757b16492702d
Matt Gibbs <mgibbs@gatech.edu>
2015-09-15 14:03:06 -0700

    Add write access support.

--- pydm/widgets/channel.py
+++ pydm/widgets/channel.py
@@ -1,8 +1,10 @@
 class PyDMChannel:
-  def __init__(self, address=None, connection_slot=None, value_slot=None, waveform_slot=None, severity_slot=None):
+  def __init__(self, address=None, connection_slot=None, value_slot=None, waveform_slot=None, severity_slot=None, write_access_slot=None, value_signal=None):
     self.address = address
     self.connection_slot = connection_slot
     self.value_slot = value_slot
     self.severity_slot = severity_slot
     self.waveform_slot = waveform_slot
+    self.write_access_slot = write_access_slot
+    self.value_signal = value_signal
     
