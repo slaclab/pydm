ce4c1838e7e5880aeecbb9e01bc7e9059b80b365
Matt Gibbs <mgibbs@slac.stanford.edu>
2015-09-22 17:25:58 -0700

    Fix EPICS plugin listener handling so the main thread doesnt hang while waiting for PVs to connect

--- pydm/epics_plugin.py
+++ pydm/epics_plugin.py
@@ -6,8 +6,8 @@ from PyQt4.QtCore import pyqtSlot, pyqtSignal, QObject, Qt, QString
 class Connection(PyDMConnection):
   def __init__(self, channel, pv, parent=None):
     super(Connection, self).__init__(channel, pv, parent)
-    self.add_listener(channel)
     self.pv = epics.PV(pv, callback=self.send_new_value, connection_callback=self.send_connection_state, form='ctrl')
+    self.add_listener(channel)
   
   def send_new_value(self, pvname=None, value=None, severity=None, count=None, write_access=None, *args, **kws):
     if count > 1:
@@ -30,9 +30,8 @@ class Connection(PyDMConnection):
     super(Connection, self).add_listener(channel)
     #If we are adding a listener to an already existing PV, we need to
     #manually send the signals indicating that the PV is connected, what the latest value is, etc.
-    if (self.listener_count > 1):
-      if self.pv.status == 1: #1 means 'connected'
-        self.send_connection_state(conn=True)
+    if epics.ca.isConnected(self.pv.chid):
+      self.send_connection_state(conn=True)
       self.pv.run_callbacks()
       
     try:
