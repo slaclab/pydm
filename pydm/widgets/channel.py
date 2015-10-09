73d0c818eccafdf9b61f33a44f60d867b0e62788
Matt Gibbs <mgibbs@gatech.edu>
2015-10-09 13:04:19 -0700

    Add support for enum string slot on channels.

--- pydm/widgets/channel.py
+++ pydm/widgets/channel.py
@@ -1,10 +1,11 @@
 class PyDMChannel:
-  def __init__(self, address=None, connection_slot=None, value_slot=None, waveform_slot=None, severity_slot=None, write_access_slot=None, value_signal=None):
+  def __init__(self, address=None, connection_slot=None, value_slot=None, waveform_slot=None, severity_slot=None, write_access_slot=None, enum_strings_slot=None, value_signal=None):
     self.address = address
     self.connection_slot = connection_slot
     self.value_slot = value_slot
     self.severity_slot = severity_slot
     self.waveform_slot = waveform_slot
     self.write_access_slot = write_access_slot
+    self.enum_strings_slot = enum_strings_slot
     self.value_signal = value_signal
     
