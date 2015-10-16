680623175386b399f63b6afe9bc62dd382b09200
Matt Gibbs <mgibbs@slac.stanford.edu>
2015-10-16 11:07:02 -0700

    Improve colormap support, allow for colormap limit changes.

--- pydm/widgets/image.py
+++ pydm/widgets/image.py
@@ -1,4 +1,4 @@
-from PyQt4.QtGui import QLabel, QApplication, QColor
+from PyQt4.QtGui import QLabel, QApplication, QColor, QActionGroup
 from PyQt4.QtCore import pyqtSignal, pyqtSlot, pyqtProperty, QString
 from pyqtgraph import ImageView
 from pyqtgraph import ImageItem
@@ -6,6 +6,10 @@ from pyqtgraph import ColorMap
 import numpy as np
 from channel import PyDMChannel
 
+color_maps = {}
+color_maps["Jet"] = np.array([[0,0,127,255],[0,0,255,255],[0,127,255,255],[0,255,255,255],[127,255,127,255],[255,255,0,255],[255,127,0,255],[255,0,0,255], [127,0,0,255]], dtype=np.ubyte)
+color_maps["Monochrome"] = np.array([[0,0,0,255],[255,255,255,255]], dtype=np.ubyte)
+color_maps["Hot"] = np.array([[0,0,0,255],[255,0,0,255],[255,127,0,255],[255,255,0,255],[255,255,255,255]], dtype=np.ubyte)
 class PyDMImageView(ImageView):
   def __init__(self, image_channel=None, width_channel=None, parent=None):
     super(PyDMImageView, self).__init__(parent)
@@ -16,16 +20,61 @@ class PyDMImageView(ImageView):
     self.ui.histogram.hide()
     self.ui.roiBtn.hide()
     self.ui.menuBtn.hide()
-    pos = np.array([0.0, 1.0/8.0, 2.0/8.0, 3.0/8.0, 4.0/8.0, 5.0/8.0, 6.0/8.0, 7.0/8.0, 1.0])
-    color = np.array([[0,0,127,255],[0,0,255,255],[0,127,255,255],[0,255,255,255],[127,255,127,255],[255,255,0,255],[255,127,0,255],[255,0,0,255], [127,0,0,255]], dtype=np.ubyte)
-    map = ColorMap(pos, color)
-    #self.lut = map.getLookupTable(0.0,1.0,256)
-    self.ui.histogram.gradient.setColorMap(map)
+    self.cm_min = 0.0
+    self.cm_max = 255.0
+    self.data_max_int = 255 #This is the max value for the image waveform's data type.  It gets set when the waveform updates.
+    self._colormapname = "Jet"
+    self._cm_colors = None
+    self.setColorMapToPreset(self._colormapname)
+    cm_menu = self.getView().getMenu(None).addMenu("Color Map")
+    cm_group = QActionGroup(self)
+    for map_name in color_maps:
+      action = cm_group.addAction(map_name)
+      action.setCheckable(True)
+      cm_menu.addAction(action)
+      if map_name == self._colormapname:
+        action.setChecked(True)
+    cm_menu.triggered.connect(self.changeColorMap)
+
+  def changeColorMap(self, action):
+    self._colormapname = str(action.text())
+    self.setColorMapToPreset(self._colormapname)
+
+  @pyqtSlot(int)
+  def setColorMapMin(self, new_min):
+    if self.cm_min != new_min:
+      self.cm_min = new_min
+      if self.cm_min > self.cm_max:
+        self.cm_max = self.cm_min
+      self.setColorMap()
+
+  @pyqtSlot(int)
+  def setColorMapMax(self, new_max):
+    if self.cm_max != new_max:
+      if new_max >= self.data_max_int:
+        new_max = self.data_max_int
+      self.cm_max = new_max
+      if self.cm_max < self.cm_min:
+        self.cm_min = self.cm_max
+      self.setColorMap()
+
+  def setColorMapToPreset(self, name):
+    self._cm_colors = color_maps[name]
+    self.setColorMap()
+
+  def setColorMap(self, map=None):
+    if not map:
+      if not self._cm_colors.any():
+        return
+      pos = np.linspace(self.cm_min/float(self.data_max_int), self.cm_max/float(self.data_max_int), num=len(self._cm_colors))
+      map = ColorMap(pos, self._cm_colors)
     self.getView().setBackgroundColor(map.map(0))
+    self.ui.histogram.gradient.setColorMap(map)
 
   @pyqtSlot(np.ndarray)
   def receiveImageWaveform(self, new_waveform):
     self.image_waveform = new_waveform
+    self.data_max_int = np.iinfo(self.image_waveform.dtype).max
     self.redrawImage()
   
   @pyqtSlot(int)
