093044f5b688a0e6a42ff7d9cf8e59e39b44d88f
Matt Gibbs <mgibbs@gatech.edu>
2016-10-14 20:10:22 -0700

    Zachary L Lentz: Improve channel documentation.

--- pydm/widgets/channel.py
+++ pydm/widgets/channel.py
@@ -4,7 +4,7 @@ class PyDMChannel:
     plugin
 
     The purpose of this class is to create a templated slot and signals list
-    that can be sent to a plugin based on the identifier placed at the
+    that can be sent to a plugin determined on the identifier placed at the
     beginning of the :attr:`.address` attribute. This allows a generic way to
     connect slots and signals to functionality within your PyDM Widget. Slots
     should be connected to functions on your created widget that perfrom
@@ -30,37 +30,46 @@ class PyDMChannel:
     :type  address: str
 
     :param connection_slot:
+    :type connection_slot: pyqtSlot        
 
     :param value_slot:
+    :type  value_slot: pyqtSlot
 
     :param severity_slot:
+    :type  severity_slot: pyqtSlot
 
     :param waveform_slot:
+    :type  waveform_slot: pyqtSlot
 
     :param write_access_slot:
+    :type  write_access_slot: pyqtSlot
 
     :param enum_strings_slot:
+    :type  enum_strings_slot: pyqtSlot
 
     :param unit_slot:
+    :type  unit_slot: pyqtSlot
 
     :param value_signal:
+    :type  value_signal: pyqtSignal
 
     :param waveform_signal:
+    :type  waveform_signal: pyqtSignal
     """
-  def __init__(self, address=None, connection_slot=None, value_slot=None, 
-               waveform_slot=None, severity_slot=None, write_access_slot=None, 
-               enum_strings_slot=None, unit_slot=None, value_signal=None, 
-               waveform_signal=None):
+    def __init__(self, address=None, connection_slot=None, value_slot=None, 
+                 waveform_slot=None, severity_slot=None, write_access_slot=None, 
+                 enum_strings_slot=None, unit_slot=None, value_signal=None, 
+                 waveform_signal=None):
 
-    self.address = address
-    
-    self.connection_slot   = connection_slot
-    self.value_slot        = value_slot
-    self.severity_slot     = severity_slot
-    self.waveform_slot     = waveform_slot
-    self.write_access_slot = write_access_slot
-    self.enum_strings_slot = enum_strings_slot
-    self.unit_slot         = unit_slot
-    
-    self.value_signal    = value_signal
-    self.waveform_signal = waveform_signal
+        self.address = address
+        
+        self.connection_slot   = connection_slot
+        self.value_slot        = value_slot
+        self.severity_slot     = severity_slot
+        self.waveform_slot     = waveform_slot
+        self.write_access_slot = write_access_slot
+        self.enum_strings_slot = enum_strings_slot
+        self.unit_slot         = unit_slot
+        
+        self.value_signal    = value_signal
+        self.waveform_signal = waveform_signal
