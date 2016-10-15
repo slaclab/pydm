af7e05ab4f74e37a8e8d2c8e6115050e25ee8bc1
Matt Gibbs <mgibbs@gatech.edu>
2016-10-14 23:51:04 -0700

    Add embedded display widget.

---
+++ pydm/widgets/embedded_display.py
@@ -0,0 +1,131 @@
+from PyQt4.QtGui import QStackedWidget
+from PyQt4.QtCore import Qt, QCoreApplication, QString, QStringList
+from PyQt4.QtCore import pyqtSignal, pyqtSlot, pyqtProperty
+
+class PyDMEmbeddedDisplay(QStackedWidget):
+    """
+    Widget to display other pydm guis inside of the current window.
+    Requires a list of filenames, either absolute paths or relative to the the
+    gui this widget is in.
+    """
+    def __init__(self, parent=None, filenames=[], initial_display=0):
+        super(PyDMEmbeddedDisplay, self).__init__(parent)
+        self.app = QCoreApplication.instance()
+        self.widget_files = []
+        self.display_filenames = filenames
+        self.setCurrentIndex(initial_display)
+
+    def update_widgets(self):
+        """
+        Remove defunct widgets and load new widgets.
+        """
+        # Block updating in designer, where only a normal QApplication exists
+        if hasattr(self.app, "open_file"):
+            i = 0
+            for i, filename in enumerate(self.display_filenames):
+                user_file = str(filename)
+                try:
+                    widget_file = self.widget_files[i]
+                except:
+                    widget_file = None
+                    self.widget_files.append(None)
+                if user_file != widget_file:
+                    new_widget = self.open_file(user_file)
+                    old_widget = self.widget(i) or None
+                    if old_widget is not None:
+                        self.app.close_widget_connections(old_widget)
+                        self.removeWidget(old_widget)
+                    self.app.establish_widget_connections(new_widget)
+                    self.insertWidget(i, new_widget)
+                    self.widget_files[i] = user_file
+            n_widgets = i+1
+            while self.count() > n_widgets:
+                widget = self.widget(n_widgets)
+                self.app.close_widget_connections(widget)
+                self.removeWidget(widget)
+            self.widget_files = self.widget_files[:i]
+
+    def open_file(self, filename):
+        """
+        Opens the widget specified in filename, relative to the file that this
+        widget is instantiated in, or absolute path.
+
+        :param filename: relative or absolute filepath
+        :type filename:  string
+        :rtyp: QWidget
+        """
+        if filename[0] == "/":
+            return self.app.open_file(filename)
+        else:
+            return self.app.open_relative(filename, self)
+
+    def update_display_at_index(self, index, new_filename):
+        """
+        Change the filename of the display at index.
+
+        :param index: index of the filename to update
+        :type index:  int
+        :param new_filename: path of the new file to use
+        :type new_filename:  QString
+        """
+        filenames = self.display_filenames
+        filenames[index] = new_filename
+        self.display_filenames = filenames
+
+    @pyqtSlot(QString)
+    def add_display(self, new_filename):
+        """
+        Append a new filename to the list of available filenames.
+
+        :param new_filename: path of the file to append
+        :type new_filename:  QString
+        """
+        filenames = self.display_filenames
+        filenames.append(new_filename)
+        self.display_filenames = filenames
+
+    @pyqtSlot(QString)
+    def edit_filename(self, name):
+        """
+        Unload the current file and load a new file.
+
+        :param name: path to the file to replace the current file
+        :type name:  QString
+        """
+        self.active_display_filename = name
+
+    @pyqtProperty(QStringList, doc=
+    """
+    List of filenames accessible through the embedded display.
+    """
+    )
+    def display_filenames(self):
+        try:
+            return self._display_filenames
+        except AttributeError:
+            return QStringList()
+
+    @display_filenames.setter
+    def display_filenames(self, filename_list):
+        self._display_filenames = QStringList()
+        for file in filename_list:
+            self._display_filenames.append(file)
+        self.update_widgets()
+
+    @pyqtProperty(QString, doc=
+    """
+    Filename of the current active display.
+    """
+    )
+    def active_display_filename(self):
+        filenames = self.display_filenames
+        index = self.currentIndex()
+        if 0 <= index < len(filenames):
+            return filenames[index]
+        return QString()
+
+    @active_display_filename.setter
+    def active_display_filename(self, filename):
+        index = self.currentIndex()
+        self.update_display_at_index(index, filename)
+
---
+++ pydm/widgets/embedded_display_qtplugin.py
@@ -0,0 +1,5 @@
+from qtplugin_base import qtplugin_factory
+from embedded_display import PyDMEmbeddedDisplay
+
+PyDMEmbeddedDisplayPlugin = qtplugin_factory(PyDMEmbeddedDisplay)
+
