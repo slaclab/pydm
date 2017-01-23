55faa63089184103e2452e45be50851ac5e4ef1f
Matt Gibbs <mgibbs@gatech.edu>
2017-01-23 10:35:33 -0800

    Change the designer plugin to not just import everything in pydm.widgets.  This change might not really be necessary.

--- designer_plugin.py
+++ designer_plugin.py
@@ -1,1 +1,14 @@
-from pydm.widgets import *
+print("Loading PyDM Widgets")
+from pydm.widgets.checkbox_qtplugin import PyDMCheckboxPlugin
+from pydm.widgets.embedded_display_qtplugin import PyDMEmbeddedDisplayPlugin
+from pydm.widgets.image_qtplugin import PyDMImageViewPlugin
+from pydm.widgets.indicator_qtplugin import PyDMIndicatorPlugin
+from pydm.widgets.label_qtplugin import PyDMLabelPlugin
+from pydm.widgets.line_edit_qtplugin import PyDMLineEditPlugin
+from pydm.widgets.pushbutton_qtplugin import PyDMPushButtonPlugin
+from pydm.widgets.related_display_button_qtplugin import PyDMRelatedDisplayButtonPlugin
+from pydm.widgets.shell_command_qtplugin import PyDMShellCommandPlugin
+from pydm.widgets.slider_qtplugin import PyDMSliderPlugin
+from pydm.widgets.timeplot_qtplugin import PyDMTimePlotPlugin
+from pydm.widgets.waveformplot_qtplugin import PyDMWaveformPlotPlugin
+from pydm.widgets.waveformtable_qtplugin import PyDMWaveformTablePlugin
