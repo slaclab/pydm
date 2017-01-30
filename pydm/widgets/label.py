8e6b8e26ce53f1be34b79fde1d40a5ef87b04e6c
Matt Gibbs <mgibbs@gatech.edu>
2017-01-30 15:42:03 -0800

    Implement alarm-sensitive borders on labels, and overhaul the alarm sensitivity code in general.  For now, alarm colors are hard-coded in the label.  Needs some thought for how to improve and generalize to other widgets.

--- pydm/widgets/label.py
+++ pydm/widgets/label.py
@@ -31,78 +31,65 @@ class PyDMLabel(QLabel):
     True: QColor(0, 0, 0,)
   }
   
+  NO_ALARM = 0x0
+  ALARM_TEXT = 0x1
+  ALARM_BORDER = 0x2
+  
+  ALARM_NONE = 0
+  ALARM_MINOR = 1
+  ALARM_MAJOR = 2
+  ALARM_INVALID = 3
+  ALARM_DISCONNECTED = 4
+  
+  #We put all this in a big dictionary to try to avoid constantly allocating and deallocating new stylesheet strings.
+  alarm_style_sheet_map = {
+    NO_ALARM: {
+      ALARM_NONE: "PyDMLabel {}",
+      ALARM_MINOR: "PyDMLabel {}",
+      ALARM_MAJOR: "PyDMLabel {}",
+      ALARM_INVALID: "PyDMLabel {}",
+      ALARM_DISCONNECTED: "PyDMLabel {}"
+    },
+    ALARM_TEXT: {
+      ALARM_NONE: "PyDMLabel {color: black;}",
+      ALARM_MINOR: "PyDMLabel {color: yellow;}",
+      ALARM_MAJOR: "PyDMLabel {color: red;}",
+      ALARM_INVALID: "PyDMLabel {color: purple;}",
+      ALARM_DISCONNECTED: "PyDMLabel {color: white;}"
+    },
+    ALARM_BORDER: {
+      ALARM_NONE: "PyDMLabel {border-width: 2px; border-style: hidden;}",
+      ALARM_MINOR: "PyDMLabel {border: 2px solid yellow;}",
+      ALARM_MAJOR: "PyDMLabel {border: 2px solid red;}",
+      ALARM_INVALID: "PyDMLabel {border: 2px solid purple;}",
+      ALARM_DISCONNECTED: "PyDMLabel {border: 2px solid white;}"
+    },
+    ALARM_TEXT | ALARM_BORDER: {
+      ALARM_NONE: "PyDMLabel {color: black; border-width: 2px; border-style: hidden;}",
+      ALARM_MINOR: "PyDMLabel {color: yellow; border: 2px solid yellow;}",
+      ALARM_MAJOR: "PyDMLabel {color: red; border: 2px solid red;}",
+      ALARM_INVALID: "PyDMLabel {color: purple; border: 2px solid purple;}",
+      ALARM_DISCONNECTED: "PyDMLabel {color: white; border: 2px solid white;}"
+    }
+  }
+  
   def __init__(self, init_channel=None, parent=None):
     super(PyDMLabel, self).__init__(parent)
     self.setTextFormat(Qt.PlainText)
     self.setTextInteractionFlags(Qt.NoTextInteraction)
-    self.setup_state_machine()
     self.value = None
     self._channels = None
     self._channel = init_channel
     self._prec = 0
+    self._alarm_sensitive_text = False
+    self._alarm_sensitive_border = True
+    self._alarm_flags = (self.ALARM_TEXT * self._alarm_sensitive_text) | (self.ALARM_BORDER * self._alarm_sensitive_border)
+    self._connected = False
     self.enum_strings = None
     self.format_string = None
     self.setText("PyDMLabel")
-    
-    
-  # Can the state machine be implemented at a lower level, like a QWidget subclass? 
-  def setup_state_machine(self):
-    self.state_machine = QStateMachine(self)
-    
-    #We'll need to talk to the parent application to figure out what colors to use for a specific state.  If the parent application doesn't have a color map (this is true when we are in Designer) then use the local colors defined above.
-    app = QApplication.instance()
-    try:
-      connection_status_color_map = app.connection_status_color_map
-      alarm_severity_color_map = app.alarm_severity_color_map
-    except AttributeError:
-      connection_status_color_map = self.local_connection_status_color_map
-      alarm_severity_color_map = self.local_alarm_severity_color_map
-    
-    #There are two connection states: Disconnected, and Connected.
-    disconnected_state = QState(self.state_machine)
-    disconnected_state.assignProperty(self, "color", connection_status_color_map[False])
-    #connected_state is parallel because it will have sub-states for alarm severity.
-    connected_state = QState(self.state_machine)
-    #connected_state itself doesn't have any particular color, that is all defined by the alarm severity.
-    
-    self.state_machine.setInitialState(disconnected_state)
-    
-    disconnected_state.addTransition(self.connected_signal, connected_state)
-    connected_state.addTransition(self.disconnected_signal, disconnected_state)
-    
-    #Now lets add the alarm severity states.
-    no_alarm_state = QState(connected_state)
-    no_alarm_state.assignProperty(self, "color", alarm_severity_color_map[0])
-    minor_alarm_state = QState(connected_state)
-    minor_alarm_state.assignProperty(self, "color", alarm_severity_color_map[1])
-    major_alarm_state = QState(connected_state)
-    major_alarm_state.assignProperty(self, "color", alarm_severity_color_map[2])
-    invalid_alarm_state = QState(connected_state)
-    invalid_alarm_state.assignProperty(self, "color", alarm_severity_color_map[3])
-    connected_state.setInitialState(no_alarm_state)
-    
-    #Add the transitions between different severities.
-    #This is a bunch, since any severity can transition to any other.
-    no_alarm_state.addTransition(self.minor_alarm_signal, minor_alarm_state)
-    no_alarm_state.addTransition(self.major_alarm_signal, major_alarm_state)
-    no_alarm_state.addTransition(self.invalid_alarm_signal, invalid_alarm_state)
-    minor_alarm_state.addTransition(self.no_alarm_signal, no_alarm_state)
-    minor_alarm_state.addTransition(self.major_alarm_signal, major_alarm_state)
-    minor_alarm_state.addTransition(self.invalid_alarm_signal, invalid_alarm_state)
-    major_alarm_state.addTransition(self.no_alarm_signal, no_alarm_state)
-    major_alarm_state.addTransition(self.minor_alarm_signal, minor_alarm_state)
-    major_alarm_state.addTransition(self.invalid_alarm_signal, invalid_alarm_state)
-    invalid_alarm_state.addTransition(self.no_alarm_signal, no_alarm_state)
-    invalid_alarm_state.addTransition(self.minor_alarm_signal, minor_alarm_state)
-    invalid_alarm_state.addTransition(self.major_alarm_signal, major_alarm_state)
-    
-    #Add a cool fade animation to a state transition.
-    #self.color_fade = QPropertyAnimation(self, "color", self)
-    #self.color_fade.setDuration(175)
-    #self.state_machine.addDefaultAnimation(self.color_fade)
-    
-    self.state_machine.start()
-    
+    self.alarmSeverityChanged(self.ALARM_DISCONNECTED)
+        
   @pyqtSlot(float)
   @pyqtSlot(int)
   @pyqtSlot(str)
@@ -125,21 +112,16 @@ class PyDMLabel(QLabel):
   def alarmStatusChanged(self, new_alarm_state):
     pass
   
-  #0 = NO_ALARM, 1 = MINOR, 2 = MAJOR, 3 = INVALID  
   @pyqtSlot(int)
   def alarmSeverityChanged(self, new_alarm_severity):
-    if new_alarm_severity == 0:
-      self.no_alarm_signal.emit()
-    elif new_alarm_severity == 1:
-      self.minor_alarm_signal.emit()
-    elif new_alarm_severity == 2:
-      self.major_alarm_signal.emit()
-    elif new_alarm_severity == 3:
-      self.invalid_alarm_signal.emit()
+    if not self._connected:
+      new_alarm_severity = self.ALARM_DISCONNECTED
+    self.setStyleSheet(self.alarm_style_sheet_map[self._alarm_flags][new_alarm_severity])
     
   #false = disconnected, true = connected
   @pyqtSlot(bool)
   def connectionStateChanged(self, connected):
+    self._connected = connected
     if connected:
       self.connected_signal.emit()
     else:
@@ -150,19 +132,32 @@ class PyDMLabel(QLabel):
     if enum_strings != self.enum_strings:
       self.enum_strings = enum_strings
       self.receiveValue(self.value)
+  
+  @pyqtProperty(bool, doc=
+  """
+  Whether or not the label's text color changes when alarm severity changes.
+  """
+  )
+  def alarmSensitiveText(self):
+    return self._alarm_sensitive_text
     
-  #Define setter and getter for the "color" property, used by the state machine to change color based on alarm severity and connection.
-  def getColor(self):
-    return self.palette().windowText().color()
-
-  def setColor(self, color):
-    palette = self.palette()
-    old_alpha = palette.windowText().color().alphaF()
-    color.setAlphaF(old_alpha)
-    palette.setColor(QPalette.WindowText, color)
-    self.setPalette(palette)
+  @alarmSensitiveText.setter
+  def alarmSensitiveText(self, checked):
+    self._alarm_sensitive_text = checked
+    self._alarm_flags = (self.ALARM_TEXT * self._alarm_sensitive_text) | (self.ALARM_BORDER * self._alarm_sensitive_border)
+  
+  @pyqtProperty(bool, doc=
+  """
+  Whether or not the label's border color changes when alarm severity changes.
+  """
+  )
+  def alarmSensitiveBorder(self):
+    return self._alarm_sensitive_border
     
-  color = pyqtProperty(QColor, getColor, setColor)
+  @alarmSensitiveBorder.setter
+  def alarmSensitiveBorder(self, checked):
+    self._alarm_sensitive_border = checked
+    self._alarm_flags = (self.ALARM_TEXT * self._alarm_sensitive_text) | (self.ALARM_BORDER * self._alarm_sensitive_border)
   
   def getChannel(self):
     return str(self._channel)
