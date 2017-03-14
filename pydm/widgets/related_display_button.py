aef689323f35c0508fa3aec1eaa88e1ec73f7336
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-03-13 17:11:01 -0700

    Add a macro property to related display buttons, takes a JSON object formatted string.

--- pydm/widgets/related_display_button.py
+++ pydm/widgets/related_display_button.py
@@ -1,5 +1,6 @@
 from ..PyQt.QtGui import QPushButton, QApplication
 from ..PyQt.QtCore import pyqtSignal, pyqtSlot, pyqtProperty, Qt
+import json
 
 class PyDMRelatedDisplayButton(QPushButton):
   __pyqtSignals__ = ("request_open_signal(str)")
@@ -11,6 +12,7 @@ class PyDMRelatedDisplayButton(QPushButton):
   def __init__(self, parent=None, filename=None):
     super(PyDMRelatedDisplayButton, self).__init__(parent)
     self._display_filename = filename
+    self._macros = {}
     self.app = QApplication.instance()
     
   def getDisplayFilename(self):
@@ -24,6 +26,30 @@ class PyDMRelatedDisplayButton(QPushButton):
     if self._display_filename != None:
       self._display_filename = None
   
+  displayFilename = pyqtProperty(str, getDisplayFilename, setDisplayFilename, resetDisplayFilename)
+  
+  def getMacros(self):
+    if len(self._macros) < 1:
+      return ""
+    return json.dumps(self._macros)
+  
+  def setMacros(self, new_macros):
+    if len(new_macros) < 1:
+      self._macros = {}
+    else:
+      try:
+        self._macros = json.loads(str(new_macros))
+      except ValueError:
+        pass
+  
+  def resetMacros(self):
+    self._macros = {}
+  
+  macros = pyqtProperty(str, getMacros, setMacros, resetMacros, doc=
+  """
+  The macro substitutions to use when launching the display, in JSON object format.
+  """)
+  
   def mouseReleaseEvent(self, mouse_event):
     if mouse_event.modifiers() == Qt.ShiftModifier:
       self.open_display(target=self.NEW_WINDOW)
@@ -36,8 +62,8 @@ class PyDMRelatedDisplayButton(QPushButton):
     if self.displayFilename == None:
       return
     if target == self.EXISTING_WINDOW:
-      self.window().go(self.displayFilename)
+      self.window().go(self.displayFilename, macros=self._macros)
     if target == self.NEW_WINDOW:
-      self.window().new_window(self.displayFilename)
+      self.window().new_window(self.displayFilename, macros=self._macros)
     
-  displayFilename = pyqtProperty(str, getDisplayFilename, setDisplayFilename, resetDisplayFilename)
+  
