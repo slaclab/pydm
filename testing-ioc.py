731861d758a4a7d29f1128361436d7d556eab0e3
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-06-24 04:29:36 -0700

    Add precision and units to Float PV in testing IOC.

--- testing-ioc.py
+++ testing-ioc.py
@@ -36,7 +36,7 @@ pvdb = {
         'TwoSpotImage'     : { 'type' : 'char', 'count': IMAGE_SIZE**2, 'value': numpy.zeros(IMAGE_SIZE**2,dtype=numpy.uint8) },
         'ImageWidth'       : { 'type' : 'int', 'value' : IMAGE_SIZE },
         'String'           : { 'type' : 'string', 'value': "Test String"},
-        'Float'            : { 'type' : 'float', 'value': 0.0, 'lolim': -1.2, 'lolo': -1.0, 'low': -0.8, 'high': 0.8, 'hihi': 1.0, 'hilim': 1.2 }
+        'Float'            : { 'type' : 'float', 'value': 0.0, 'lolim': -1.2, 'lolo': -1.0, 'low': -0.8, 'high': 0.8, 'hihi': 1.0, 'hilim': 1.2, 'units': 'mJ', 'prec': 3 }
 }
 
 def double_gaussian_2d(x, y, x0, y0, xsig, ysig):
