2ba73c8df5dd9575cefefa7de0e8377b6134e30e
Alexander <alexander.rischka@mpi-hd.mpg.de>
2017-06-28 10:39:17 +0200

    Fix image colormap, slider value error, pyepics_plugin list error

--- pydm/data_plugins/pyepics_plugin.py
+++ pydm/data_plugins/pyepics_plugin.py
@@ -23,7 +23,10 @@ class Connection(PyDMConnection):
       self.unit_signal.emit(units)
     if value is not None:
       if count > 1:
-        self.new_waveform_signal.emit(value)
+        if isinstance(value, list): # this is a workaround for lists and I'm
+          self.new_value_signal[str].emit(str(value)) # not sure if this is safe at all
+        else:
+          self.new_waveform_signal.emit(value)
       else:
         if ftype in (epics.dbr.INT, epics.dbr.CTRL_INT, epics.dbr.TIME_INT, epics.dbr.ENUM, epics.dbr.CTRL_ENUM, epics.dbr.TIME_ENUM):
           self.new_value_signal[int].emit(int(value))
