f05c989859691eeed04372822cd22c1b8932c51c
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-07-18 13:02:28 -0700

    Add QItemSelection to both QtCore and QtGui, so that people used to both PyQt4 and PyQt5 can import it from where they expect.

--- pydm/PyQt/QtGui.py
+++ pydm/PyQt/QtGui.py
@@ -5,3 +5,5 @@ if QT_LIB == 'PyQt4':
 elif QT_LIB == 'PyQt5':
     from PyQt5.QtGui import *
     from PyQt5.QtWidgets import *
+    #Crappy work around:  QItemSelection exists in QtGui for PyQt4 but QtCore for PyQt5.  We'll make it available in both.
+    from PyQt5.QtCore import QItemSelection
