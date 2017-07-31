db8a6dcea24138e14e90b24a12c959988e4e728a
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-07-31 10:01:50 -0700

    Move EPICS plugin selection to data_plugins.

---
+++ pydm/data_plugins/__init__.py
@@ -0,0 +1,5 @@
+from .epics_plugin import EPICSPlugin
+from .archiver_plugin import ArchiverPlugin
+from .fake_plugin import FakePlugin
+
+__all__ = ["EPICSPlugin", "ArchiverPlugin", "FakePlugin"]
---
+++ pydm/data_plugins/epics_plugin.py
@@ -0,0 +1,19 @@
+#If the user has PSP and pyca installed, use psp, which is faster.
+#Otherwise, use PyEPICS, which is slower, but more commonly used.
+#To force a particular library, set the PYDM_EPICS_LIB environment
+#variable to either pyepics or pyca.
+import os
+EPICS_LIB = os.getenv("PYDM_EPICS_LIB")
+if EPICS_LIB == "pyepics":
+  from .pyepics_plugin import PyEPICSPlugin
+  EPICSPlugin = PyEPICSPlugin
+elif EPICS_LIB == "pyca":
+  from .psp_plugin import PSPPlugin
+  EPICSPlugin = PSPPlugin
+else:
+  try:
+    from .psp_plugin import PSPPlugin
+    EPICSPlugin = PSPPlugin
+  except ImportError:
+    from .pyepics_plugin import PyEPICSPlugin
+    EPICSPlugin = PyEPICSPlugin
