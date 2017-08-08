3fc28c3d74d2ffa845a3c40b00db365679ce33e6
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-08-08 15:07:08 -0700

    Adjusting PyDMLabel after changes on the base class.

--- pydm/widgets/label.py
+++ pydm/widgets/label.py
@@ -11,14 +11,17 @@ class PyDMLabel(QLabel, PyDMWidget):
     
     def value_changed(self, new_value):
         super().value_changed(new_value)
+        #If the value is a string, just display it as-is, no formatting needed.
         if isinstance(new_value, str):
             self.setText(new_value)
             return
-        if isinstance(new_value, float):
-            if self.format_string:
-                self.setText(self.format_string.format(new_value))
-                return
+        #If the value is an enum, display the appropriate enum string for the value.
         if self.enum_strings is not None and isinstance(new_value, int):
             self.setText(self.enum_strings[new_value])
             return
+        #If the value is a number (float or int), display it using a format string if necessary.
+        if isinstance(new_value, float) or isinstance(new_value, int):
+            self.setText(self.format_string.format(new_value))
+            return
+        #If you made it this far, just turn whatever the heck the value is into a string and display it.
         self.setText(str(new_value))
