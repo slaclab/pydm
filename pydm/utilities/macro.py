90fa3385b73dfd0a0626540c99c02747801fb00b
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-08-07 23:01:28 -0700

    STY: 2 - 4 spaces at utilities

--- pydm/utilities/macro.py
+++ pydm/utilities/macro.py
@@ -3,9 +3,9 @@ import os
 from string import Template
 
 def substitute_in_file(file_path, macros):
-  temporary_fd, temp_filename = tempfile.mkstemp()
-  with open(file_path) as orig_file:
-    with os.fdopen(temporary_fd, 'w') as temporary_file:
-      t = Template(orig_file.read())
-      temporary_file.write(t.safe_substitute(macros))
-  return temp_filename
+    temporary_fd, temp_filename = tempfile.mkstemp()
+    with open(file_path) as orig_file:
+        with os.fdopen(temporary_fd, 'w') as temporary_file:
+            t = Template(orig_file.read())
+            temporary_file.write(t.safe_substitute(macros))
+    return temp_filename
