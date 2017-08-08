e38526def5bcb1d175854cb6c6ce98ecd054066f
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-08-07 22:59:02 -0700

    STY: 2 - 4 spaces at display_module and main_window.py

--- pydm/display_module.py
+++ pydm/display_module.py
@@ -4,22 +4,22 @@ from .PyQt.QtGui import QWidget
 from .utilities import macro
 
 class Display(QWidget):
-  def __init__(self, parent=None, args=None, macros=None):
-    super(Display, self).__init__(parent=parent)
-    self.ui = None
-    self.load_ui(parent=parent, macros=macros)
-  
-  def ui_filepath(self):
-    raise NotImplementedError
-      
-  def ui_filename(self):
-    raise NotImplementedError
-  
-  def load_ui(self, parent=None, macros=None):
-    if self.ui:
-      return self.ui
-    if macros is not None:
-      f = macro.substitute_in_file(self.ui_filepath(), macros)
-    else:
-      f = self.ui_filepath()
-    self.ui = uic.loadUi(f, baseinstance=self)
+    def __init__(self, parent=None, args=None, macros=None):
+        super(Display, self).__init__(parent=parent)
+        self.ui = None
+        self.load_ui(parent=parent, macros=macros)
+    
+    def ui_filepath(self):
+        raise NotImplementedError
+            
+    def ui_filename(self):
+        raise NotImplementedError
+    
+    def load_ui(self, parent=None, macros=None):
+        if self.ui:
+            return self.ui
+        if macros is not None:
+            f = macro.substitute_in_file(self.ui_filepath(), macros)
+        else:
+            f = self.ui_filepath()
+        self.ui = uic.loadUi(f, baseinstance=self)
