e83b71f9f4591651a447c307780e7d34aaeeac50
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-08-14 10:13:51 -0700

    Removing unused imports and x_data that was also never used.

--- pydm/data_plugins/archiver_plugin.py
+++ pydm/data_plugins/archiver_plugin.py
@@ -1,7 +1,5 @@
 from .plugin import PyDMPlugin, PyDMConnection
-from ..PyQt.QtCore import pyqtSlot, pyqtSignal, QObject, Qt
 import requests
-import json
 import numpy as np
 
 class Connection(PyDMConnection):
@@ -12,7 +10,8 @@ class Connection(PyDMConnection):
         r = requests.get(url_string) #blocking.  BAD!
         if r.status_code == 200 and r.headers['content-type'] == 'application/json':
             data_dict = r.json()
-            x_data = np.array([point["secs"] for point in data_dict[0]["data"]])
+            #x_data not used so commented out... maybe return it with y_data?
+            #x_data = np.array([point["secs"] for point in data_dict[0]["data"]])
             y_data = np.array([point["val"] for point in data_dict[0]["data"]])
             self.new_waveform_signal.emit(y_data)
             
