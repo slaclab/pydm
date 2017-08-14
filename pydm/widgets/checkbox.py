8ff48403ab97464c3d766fcbdf3e381eaeb9fff9
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-08-14 10:48:44 -0700

    DOC: Docstrings for checkbox widget.

--- pydm/widgets/checkbox.py
+++ pydm/widgets/checkbox.py
@@ -2,11 +2,31 @@ from ..PyQt.QtGui import QCheckBox
 from .base import PyDMWritableWidget
 
 class PyDMCheckbox(QCheckBox, PyDMWritableWidget):    
+    """
+    A QCheckbox with support for Channels and more from PyDM
+    
+    Parameters
+    ----------
+    parent : QWidget
+        The parent widget for the Label
+    init_channel : str, optional
+        The channel to be used by the widget. 
+    
+    """
     def __init__(self, parent=None, init_channel=None):
         super(PyDMCheckbox, self).__init__(parent, init_channel=init_channel)
         self.clicked.connect(self.send_value)
     
     def value_changed(self, new_val):
+        """
+        Callback invoked when the Channel value is changed.
+        Sets the checkbox checked or not based on the new value.
+        
+        Parameters
+        ----------
+        new_val : int
+            The new value from the channel.
+        """
         super().value_changed(new_val)
         if new_val is None:
             return
@@ -16,6 +36,15 @@ class PyDMCheckbox(QCheckBox, PyDMWritableWidget):
             self.setChecked(False)
     
     def send_value(self, checked):
+        """
+        Method that emit the signal to notify the Channel that a new value was 
+        written at the widget.
+        
+        Parameters
+        ----------
+        checked : bool
+            True in case the checkbox was checked, False otherwise.
+        """
         if checked:
             self.send_value_signal.emit(1)
         else:
