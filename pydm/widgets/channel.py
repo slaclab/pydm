97aa0d26050e994363253ac9ec291bc356ce66d4
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-08-31 09:09:33 -0700

    Deprecating waveformslot and waveformsignal at channel.

--- pydm/widgets/channel.py
+++ pydm/widgets/channel.py
@@ -43,9 +43,6 @@ class PyDMChannel:
     severity_slot : pyqtSlot, optional
         A function to be run when the severity changes
 
-    waveform_slot : pyqtSlot, optional
-        A function to be run when the waveform value changes
-
     write_access_slot : pyqtSlot, optional
         A function to be run when the write access changes
 
@@ -62,21 +59,18 @@ class PyDMChannel:
         Attach a signal here that emits a desired value to be sent through the
         plugin
 
-    waveform_signal : pyqtSlot, optional
-        Attach a signal here that emits a desired waveform
     """
     def __init__(self, address=None, connection_slot=None, value_slot=None, 
-                 waveform_slot=None, severity_slot=None, write_access_slot=None, 
+                 severity_slot=None, write_access_slot=None, 
                  enum_strings_slot=None, unit_slot=None, prec_slot=None,
                  upper_ctrl_limit_slot = None, lower_ctrl_limit_slot = None,
-                 value_signal=None, waveform_signal=None):
+                 value_signal=None):
 
         self.address = address
         
         self.connection_slot   = connection_slot
         self.value_slot        = value_slot
         self.severity_slot     = severity_slot
-        self.waveform_slot     = waveform_slot
         self.write_access_slot = write_access_slot
         self.enum_strings_slot = enum_strings_slot
         self.unit_slot         = unit_slot
@@ -86,4 +80,3 @@ class PyDMChannel:
         self.lower_ctrl_limit_slot = lower_ctrl_limit_slot
         
         self.value_signal    = value_signal
-        self.waveform_signal = waveform_signal
