13921b1d835957b8b3cb005b3fc7e58d05cdf9ff
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-09-20 14:06:05 -0700

    DOC: Docs for related display button.

--- pydm/widgets/related_display_button.py
+++ pydm/widgets/related_display_button.py
@@ -4,8 +4,18 @@ import json
 from .base import PyDMWidget, compose_stylesheet
 
 class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
-    __pyqtSignals__ = ("request_open_signal(str)")
+    """
+    A QPushButton capable of opening a new Display at the same of at a
+    new window.
 
+    Parameters
+    ----------
+    init_channel : str, optional
+        The channel to be used by the widget.
+
+    filename : str, optional
+        The file to be opened
+    """
     # Constants for determining where to open the display.
     EXISTING_WINDOW = 0;
     NEW_WINDOW = 1;
@@ -33,6 +43,21 @@ class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
         self.app = QApplication.instance()
 
     def alarm_severity_changed(self, new_alarm_severity):
+        """
+        Callback invoked when the Channel alarm severity is changed.
+        This callback is not processed if the widget has no channel
+        associated with it.
+        This callback handles the composition of the stylesheet to be
+        applied and the call
+        to update to redraw the widget with the needed changes for the
+        new state.
+
+        Parameters
+        ----------
+        new_alarm_severity : int
+            The new severity where 0 = NO_ALARM, 1 = MINOR, 2 = MAJOR
+            and 3 = INVALID
+        """
         PyDMWidget.alarm_severity_changed(self, new_alarm_severity)
         if self._channels is not None:
             self._alarm_state = new_alarm_severity
@@ -45,21 +70,46 @@ class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
 
     @pyqtProperty(str)
     def text(self):
+        """
+        The QPushButton text property
+
+        Returns
+        -------
+        str
+        """
         return self.push_button.text()
 
     @text.setter
     def text(self, txt):
+        """
+        The QPushButton text property
+
+        Parameters
+        ----------
+        txt : str
+        """
         self.push_button.setText(txt)
 
     @pyqtProperty(str)
     def displayFilename(self):
         """
         The filename to open
+
+        Returns
+        -------
+        str
         """
         return str(self._display_filename)
 
     @displayFilename.setter
     def displayFilename(self, value):
+        """
+        The filename to open
+
+        Parameters
+        ----------
+        value : str
+        """
         if self._display_filename != value:
             self._display_filename = str(value)
             if self._display_filename is None or len(self._display_filename) < 1:
@@ -69,6 +119,10 @@ class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
     def macros(self):
         """
         The macro substitutions to use when launching the display, in JSON object format.
+
+        Returns
+        -------
+        str
         """
         if self._macro_string is None:
             return ""
@@ -76,6 +130,13 @@ class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
 
     @macros.setter
     def macros(self, new_macros):
+        """
+        The macro substitutions to use when launching the display, in JSON object format.
+
+        Parameters
+        ----------
+        new_macros : str
+        """
         if len(new_macros) < 1:
             self._macro_string = None
         else:
@@ -85,14 +146,40 @@ class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
     def openInNewWindow(self):
         """
         If true, the button will open the display in a new window, rather than in the existing window.
+
+        Returns
+        -------
+        bool
         """
         return self._open_in_new_window
 
     @openInNewWindow.setter
     def openInNewWindow(self, open_in_new):
+        """
+        If true, the button will open the display in a new window, rather than in the existing window.
+
+        Parameters
+        ----------
+        open_in_new : bool
+        """
         self._open_in_new_window = open_in_new
 
     def push_button_release_event(self, mouse_event):
+        """
+        Opens the related display given by `filename`.
+        If the Shift Key is hold it will open in a new window.
+
+        Called when a mouse button is released. A widget receives mouse
+        release events when it has received the corresponding mouse press
+        event. This means that if the user presses the mouse inside your
+        widget, then drags the mouse somewhere else before releasing the
+        mouse button, your widget receives the release event.
+
+        Parameters
+        ----------
+        mouse_event : QMouseEvent
+
+        """
         if mouse_event.modifiers() == Qt.ShiftModifier or self._open_in_new_window:
             self.open_display(target=self.NEW_WINDOW)
         else:
@@ -101,6 +188,16 @@ class PyDMRelatedDisplayButton(QFrame, PyDMWidget):
 
     @pyqtSlot()
     def open_display(self, target=EXISTING_WINDOW):
+        """
+        Open the configured `filename` with the given `target`.
+
+        Parameters
+        ----------
+        target : int
+            PyDMRelatedDisplayButton.EXISTING_WINDOW or 0 will open the
+            file on the same window. PyDMRelatedDisplayButton.NEW_WINDOW
+            or 1 will result on a new process.
+        """
         if self.displayFilename is None:
             return
         macros = None
