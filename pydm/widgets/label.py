d5657d549a138ae8000f38d3ec70993aea784ece
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-10-03 10:24:12 -0700

    WIP: Display Format at Label.

--- pydm/widgets/label.py
+++ pydm/widgets/label.py
@@ -1,8 +1,24 @@
 from .base import PyDMWidget
 from ..PyQt.QtGui import QLabel
-from ..PyQt.QtCore import Qt
+from ..PyQt.QtCore import Qt, pyqtProperty, Q_ENUMS
+
+try:
+    # unichr is not available on Py3+
+    unichr(1)
+except NameError:
+    unichr = chr
 
 class PyDMLabel(QLabel, PyDMWidget):
+    class DisplayFormat:
+        DEFAULT = 0
+        STRING = 1
+        DECIMAL = 2
+        EXPONENTIAL = 3
+        HEX = 4
+        BINARY = 5
+    
+    Q_ENUMS(DisplayFormat)
+    
     """
     A QLabel with support for Channels and more from PyDM
 
@@ -19,6 +35,47 @@ class PyDMLabel(QLabel, PyDMWidget):
         self.setTextFormat(Qt.PlainText)
         self.setTextInteractionFlags(Qt.NoTextInteraction)
         self.setText("PyDMLabel")
+        self._display_format_type = self.DisplayFormat.DEFAULT
+
+    @pyqtProperty(DisplayFormat)
+    def displayFormat(self):
+        return self._display_format_type
+
+    @displayFormat.setter
+    def displayFormat(self, new_type):
+        if self._display_format_type != new_type:
+            self._display_format_type = new_type
+
+    def parse_value_for_display(self, new_value):
+        if self._display_format_type == self.DisplayFormat.DEFAULT:
+            return new_value
+        elif self._display_format_type == self.DisplayFormat.STRING:
+            if isinstance(new_value, str):
+                return new_value
+            fmt_string = "{}"*len(new_value)
+            r = fmt_string.format(*[unichr(x) for x in new_value])
+            return r
+        elif self._display_format_type == self.DisplayFormat.DECIMAL:
+            # This case is taken care by the current string formatting 
+            # routine
+            return r
+        elif self._display_format_type == self.DisplayFormat.EXPONENTIAL:
+            fmt_string = "{"+":.{}e".format(self._prec)+"}"
+            try:
+                r = fmt_string.format(new_value)
+            except ValueError:
+                print("Could not format value {} to exponential.".format(new_value))
+                r = new_value
+            return r
+        elif self._display_format_type == self.DisplayFormat.HEX:
+            # TODO: Implement Hexadecimal formating
+            fmt_string = "{:#0X}"
+            try:
+                r = fmt_string.format(new_value)
+            except ValueError:
+                print("Could not format value {} to hex.".format(new_value))
+                r = new_value
+            return r
 
     def value_changed(self, new_value):
         """
