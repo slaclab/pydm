c0591ed76b18239903b2ce9c314e42dbff23509f
Matt Gibbs <mattgibbs@users.noreply.github.com>
2017-10-03 02:03:44 -0700

    New Navigation Bar (#117)
    * WIP: Replace old navigation widgets with a new toolbar.
    
    * WIP replace buttons with action in Main Window.
    
    * Use HiDPI in PyDM if available.
    
    * Add an IconFont utility class that makes QIcons from icon font characters.
    
    * Use new QToolbar-based navigation bar, and update menus with actions for navigation.
    
    * Set loaded displays to be the centralWidget of MainWindow, rather than adding to a layout.
    
    * Use PyQt compatibility layer instead of just directly importing from PyQt5.
    
    * Simplify iconfont module.
    
    * Fix python3 compatibility issue, and fix broken CharIconEngine.paint method.
    
    * Add a command line option to hide the navbar.
    
    * Set a minimum width on the main window.
    
    * Add icon fonts to MANIFEST.in

--- pydm/utilities/__init__.py
+++ pydm/utilities/__init__.py
@@ -2,6 +2,7 @@ from .units import find_unittype, convert, find_unit_options
 from . import macro
 from . import colors
 from .remove_protocol import remove_protocol
+from .iconfont import IconFont
 
 import os
 import sys
