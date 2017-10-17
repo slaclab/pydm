82555ec4f9106feacb8620e96684e87333111867
fernandohds564 <fernandohds564@gmail.com>
2017-10-17 02:23:27 -0200

    PR: add new units to utilities (#140)
    * Add angle, voltage and current unit conversions to unit.py.
    
    * Add more units and unit types.
    
    * Do not export find_unit method in utilities package.

--- pydm/utilities/units.py
+++ pydm/utilities/units.py
@@ -1,30 +1,58 @@
 from scipy import constants
 
-
-UNITS = {'length':   {'m'   : 1,
-                      'cm'  : constants.centi,
-                      'mm'  : constants.milli,
-                      'um'  : constants.micro,
-                      'nm'  : constants.nano,
-                      'pm'  : constants.pico,
-                      'in'  : constants.inch,
-                      'ft'  : constants.foot,
-                      'yds' : constants.yard,
+UNITS = {'length':   {'m': 1,
+                      'cm': constants.centi,
+                      'mm': constants.milli,
+                      'um': constants.micro,
+                      'nm': constants.nano,
+                      'pm': constants.pico,
+                      'in': constants.inch,
+                      'ft': constants.foot,
+                      'yds': constants.yard,
+                      },
+         'time':    {'s': 1,
+                     'ms': constants.milli,
+                     'us': constants.micro,
+                     'ns': constants.nano,
+                     'ps': constants.pico,
+                     'min': constants.minute,
+                     'hr': constants.hour,
+                     'weeks': constants.week,
+                     'days': constants.day,
                      },
-          'time':    {'s'     : 1,
-                      'ms'    : constants.milli,
-                      'us'    : constants.micro,
-                      'ns'    : constants.nano,
-                      'min'   : constants.minute,
-                      'hr'    : constants.hour,
-                      'weeks' : constants.week,
-                      'days'  : constants.day,
+         'frequency': {'Hz': 1,
+                       'kHz': constants.kilo,
+                       'MHz': constants.mega,
+                       'GHz': constants.giga,
+                       'THz': constants.tera,
+                       'mHz': constants.milli,
+                       },
+         'angle':   {'rad': 1,
+                     'mrad': constants.milli,
+                     'urad': constants.micro,
+                     'nrad': constants.nano,
+                     'degree': constants.degree,
+                     'turn': 2*constants.pi,
+                     },
+         'voltage': {'V': 1,
+                     'MV': constants.mega,
+                     'kV': constants.kilo,
+                     'mV': constants.milli,
+                     'uV': constants.micro,
+                     },
+         'current': {'A': 1,
+                     'MA': constants.mega,
+                     'kA': constants.kilo,
+                     'mA': constants.milli,
+                     'uA': constants.micro,
+                     'nA': constants.nano,
                      }
-        }
+         }
+
 
 def find_unittype(unit):
     """
-    Find the type of a unit string
+    Find the type of a unit string.
     """
     for tp in UNITS.keys():
         if unit in UNITS[tp].keys():
@@ -34,7 +62,7 @@ def find_unittype(unit):
 
 def find_unit(unit):
     """
-    Find the conversion of a unit string
+    Find the conversion of a unit string.
     """
     tp = find_unittype(unit)
     if tp:
@@ -43,30 +71,31 @@ def find_unit(unit):
         return None
 
 
-def convert(unit,desired):
+def convert(unit, desired):
     """
-    Find the conversion rate of two different unit strings
+    Find the conversion rate of two different unit strings.
     """
     current = find_unit(unit)
-    final   = find_unit(desired)
+    final = find_unit(desired)
 
     if find_unittype(unit) != find_unittype(desired):
         return None
 
     if current and final:
         return current/final
-    
+
     else:
         return None
 
+
 def find_unit_options(unit):
     """
-    Find the options for a given unit
+    Find the options for a given unit.
     """
     tp = find_unittype(unit)
     if tp:
-        units = [choice for choice,_ in 
-                 sorted(UNITS[tp].items(),key=lambda x: 1/x[1])]
+        units = [choice for choice, _ in
+                 sorted(UNITS[tp].items(), key=lambda x: 1/x[1])]
         return units
     else:
         return None
