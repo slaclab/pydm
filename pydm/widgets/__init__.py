e8ee2225337bc722f95f4c130b00c94bfcc97132
Hugo Slepicka <slepicka@slac.stanford.edu>
2017-11-02 10:46:20 -0700

    ENH: Bring widgets import to __init__.py making it easier for users.

---
+++ pydm/widgets/__init__.py
@@ -0,0 +1,24 @@
+from .byte import PyDMByteIndicator
+
+from .checkbox import PyDMCheckbox
+from .drawing import (PyDMDrawingLine, PyDMDrawingRectangle, PyDMDrawingTriangle,
+                      PyDMDrawingEllipse, PyDMDrawingCircle, PyDMDrawingArc,
+                      PyDMDrawingPie, PyDMDrawingChord, PyDMDrawingImage)
+
+from .embedded_display import PyDMEmbeddedDisplay
+from .enum_combo_box import PyDMEnumComboBox
+from .image import PyDMImageView
+from .indicator import PyDMIndicator
+from .label import PyDMLabel
+from .line_edit import PyDMLineEdit
+from .pushbutton import PyDMPushButton
+from .related_display_button import PyDMRelatedDisplayButton
+from .shell_command import PyDMShellCommand
+from .slider import PyDMSlider
+from .spinbox import PyDMSpinbox
+from .symbol import PyDMSymbol
+from .waveformtable import PyDMWaveformTable
+from .scale import PyDMScaleIndicator
+from .timeplot import PyDMTimePlot
+from .waveformplot import PyDMWaveformPlot
+from .scatterplot import PyDMScatterPlot
