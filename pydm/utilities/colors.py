6e31717fd3965de5302029f2f6e178837b868e62
Fernando <fernandohds564@gmail.com>
2017-11-10 14:55:53 -0200

    fix formatting of colors module.

--- pydm/utilities/colors.py
+++ pydm/utilities/colors.py
@@ -9,7 +9,8 @@ with open(os.path.join(current_dir, 'hex2color.pkl'), 'rb') as f:
     hex_to_svg_color_map = pickle.load(f)
 with open(os.path.join(current_dir, 'color2hex.pkl'), 'rb') as f:
     svg_color_to_hex_map = pickle.load(f)
-    
+
+
 def svg_color_from_hex(hex_string, hex_on_fail=False):
     if not hex_on_fail:
         return hex_to_svg_color_map[str(hex_string).lower()]
@@ -17,8 +18,13 @@ def svg_color_from_hex(hex_string, hex_on_fail=False):
         return hex_to_svg_color_map[str(hex_string).lower()]
     except KeyError:
         return hex_string
-    
+
+
 def hex_from_svg_color(color_string):
     return svg_color_to_hex_map[str(color_string).lower()]
 
-default_colors = ['white', 'red', 'dodgerblue', 'forestgreen', 'yellow', 'fuchsia', 'turquoise', 'deeppink', 'lime', 'orange', 'whitesmoke', 'beige', 'purple', 'teal', 'darksalmon', 'brown']
+
+default_colors = ['white', 'red', 'dodgerblue', 'forestgreen', 'yellow',
+                  'fuchsia', 'turquoise', 'deeppink', 'lime', 'orange',
+                  'whitesmoke', 'beige', 'purple', 'teal', 'darksalmon',
+                  'brown']
