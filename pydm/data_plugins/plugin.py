9d251b9a2d5ddb49ac876fba9155f8cb5138da5b
Zachary Lentz <zlentz@slac.stanford.edu>
2017-11-28 09:19:55 -0800

    FIX: Clean up signal/slot connections in plugins This ensures that the slot methods never get called once we've cleaned up the connection. Otherwise, we get issues with two PVs connected to one widget if we try to switch a widget's channel.

--- pydm/data_plugins/plugin.py
+++ pydm/data_plugins/plugin.py
@@ -59,7 +59,48 @@ class PyDMConnection(QObject):
         if channel.prec_slot is not None:
             self.prec_signal.connect(channel.prec_slot, Qt.QueuedConnection)
       
-    def remove_listener(self):
+    def remove_listener(self, channel):
+        if channel.connection_slot is not None:
+            self.connection_state_signal.disconnect(channel.connection_slot)
+        if channel.value_slot is not None:
+            try:
+                self.new_value_signal[int].disconnect(channel.value_slot)
+            except TypeError:
+                pass
+            try:
+                self.new_value_signal[float].disconnect(channel.value_slot)
+            except TypeError:
+                pass
+            try:
+                self.new_value_signal[str].disconnect(channel.value_slot)
+            except TypeError:
+                pass
+            try:
+                self.new_value_signal[ndarray].disconnect(channel.value_slot)
+            except TypeError:
+                pass
+
+        if channel.severity_slot is not None:
+            self.new_severity_signal.disconnect(channel.severity_slot)
+
+        if channel.write_access_slot is not None:
+            self.write_access_signal.disconnect(channel.write_access_slot)
+
+        if channel.enum_strings_slot is not None:
+            self.enum_strings_signal.disconnect(channel.enum_strings_slot)
+
+        if channel.unit_slot is not None:
+            self.unit_signal.disconnect(channel.unit_slot)
+
+        if channel.upper_ctrl_limit_slot is not None:
+            self.upper_ctrl_limit_signal.disconnect(channel.upper_ctrl_limit_slot)
+
+        if channel.lower_ctrl_limit_slot is not None:
+            self.lower_ctrl_limit_signal.disconnect(channel.lower_ctrl_limit_slot)
+
+        if channel.prec_slot is not None:
+            self.prec_signal.disconnect(channel.prec_slot)
+ 
         self.listener_count = self.listener_count - 1
         if self.listener_count < 1:
             self.close()
@@ -86,6 +127,6 @@ class PyDMPlugin(object):
     def remove_connection(self, channel):
         address = self.get_address(channel)
         if address in self.connections:
-            self.connections[address].remove_listener()
+            self.connections[address].remove_listener(channel)
             if self.connections[address].listener_count < 1:
                 del self.connections[address]
