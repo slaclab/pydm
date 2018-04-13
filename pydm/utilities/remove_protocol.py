691a858c1cf2050db540de2f05c2dc03f8685077
Hugo Slepicka <slepicka@slac.stanford.edu>
2018-04-13 16:04:40 -0700

    FIX: remove_protocol was removing all occurrences of :// when the correct behaviour is to remove only the first and return the correct address pieces.

--- pydm/utilities/remove_protocol.py
+++ pydm/utilities/remove_protocol.py
@@ -1,6 +1,17 @@
 def remove_protocol(addr):
-    try:
-        name = addr.split("://")[1]
-    except IndexError:
-        name = addr
+    """
+    Removes the first occurrence of the protocol string ('://') from the string `addr`
+
+    Parameters
+    ----------
+    addr : str
+        The address from which to remove the address prefix.
+
+    Returns
+    -------
+    str
+    """
+
+    name = addr.split("://", 1)  # maxsplit = 1... removes only the first occurrence
+    name = ''.join(name[1:]) if len(name) > 1 else addr
     return name
