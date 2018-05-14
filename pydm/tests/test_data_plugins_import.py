16cf4812a0a1af0b2905d89e47da9544d377f9d2
teddyrendahl <trendahl@slac.stanford.edu>
2018-05-14 15:49:26 -0700

    TST: Check class type for add_plugin

--- pydm/tests/test_data_plugins_import.py
+++ pydm/tests/test_data_plugins_import.py
@@ -3,12 +3,12 @@ from pydm.data_plugins import add_plugin, PyDMPlugin, plugin_modules
 
 def test_data_plugin_add(qapp):
     # Create test PyDMPlugin with mock protocol
-    test_plug = PyDMPlugin()
+    test_plug = PyDMPlugin
     test_plug.protocol = 'tst'
     # Check that adding this after import will be reflected in PyDMApp
     add_plugin(test_plug)
-    assert plugin_modules['tst'] == test_plug
-    assert qapp.plugins['tst'] == test_plug
+    assert isinstance(plugin_modules['tst'], test_plug)
+    assert isinstance(qapp.plugins['tst'], test_plug)
 
 
 def test_default_plugin_loading(qapp):
