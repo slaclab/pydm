27ed89887188c405732937ab6e72c5eee46e54e9
Hugo Slepicka <slepicka@slac.stanford.edu>
2018-06-03 23:09:27 -0700

    ENH: Add  method to Image View so users can easily hook-up data processing code of their own. MNT: Refactored the image processing example to work with the new callback.

--- examples/image_processing/image_view.py
+++ examples/image_processing/image_view.py
@@ -7,43 +7,36 @@ from skimage.feature import blob_doh
 from marker import ImageMarker
 from pyqtgraph import mkPen
 
+
 class ImageViewer(Display):
     def __init__(self, parent=None, args=None):
         super(ImageViewer, self).__init__(parent=parent, args=args)
-        self.image_channel = "ca://MTEST:TwoSpotImage"
-        self.image_width_channel = "ca://MTEST:ImageWidth"
-        self.ui.imageView.widthChannel = self.image_width_channel
-        self.ui.imageView.image_channel = ""
+        self.ui.imageView.process_image = self.process_image
         self.markers = []
-        
+
     def ui_filename(self):
         return 'image_view.ui'
-        
+
     def ui_filepath(self):
         return path.join(path.dirname(path.realpath(__file__)), self.ui_filename())
-    
-    @pyqtSlot(np.ndarray)
-    def new_image_received(self, new_waveform):
-        #Reshape the 1D waveform into 2D
-        img = new_waveform.reshape((int(512),-1), order='C')
-        #Find blobs in the image with scikit-image
-        blobs= blob_doh(img, max_sigma=512, min_sigma=64, threshold=.02)
-        #Remove any existing blob markers        
+
+    def process_image(self, new_image):
+        # Find blobs in the image with scikit-image
+        blobs = blob_doh(new_image, max_sigma=512, min_sigma=64, threshold=.02)
+        # Remove any existing blob markers
         for m in self.markers:
             self.ui.imageView.getView().removeItem(m)
         self.markers = []
-        #For each blob, add a blob marker to the image
+        # For each blob, add a blob marker to the image
         for blob in blobs:
             x, y, size = blob
-            m = ImageMarker((y,x), size=size, pen=mkPen((100,100,255), width=3))
+            m = ImageMarker((y, x), size=size, pen=mkPen((100, 100, 255), width=3))
             self.ui.imageView.getView().addItem(m)
             self.markers.append(m)
-        #Show number of blobs in text label  
+        # Show number of blobs in text label
         self.ui.numBlobsLabel.setText(str(len(blobs)))
-        #Send the original image data to the image widget
-        self.ui.imageView.image_value_changed(new_waveform)
-    
-    def channels(self):
-        return [PyDMChannel(address=self.image_channel, value_slot=self.new_image_received)]
+        # Send the original image data to the image widget
+        return new_image
+
 
 intelclass = ImageViewer
