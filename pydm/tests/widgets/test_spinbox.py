7410656a37851ef27eee89ce087b049118a8203a
Fernando <fernandohds564@gmail.com>
2018-10-20 12:52:55 -0300

    STY: Fix style errors in test_spinbox.py file.

--- pydm/tests/widgets/test_spinbox.py
+++ pydm/tests/widgets/test_spinbox.py
@@ -3,9 +3,9 @@
 
 import pytest
 
-from qtpy.QtWidgets import QApplication, QDoubleSpinBox
+from qtpy.QtWidgets import QApplication
 from qtpy.QtGui import QKeyEvent
-from qtpy.QtCore import Property, QEvent, Qt
+from qtpy.QtCore import QEvent, Qt
 
 from ...widgets.spinbox import PyDMSpinbox
 from ...tests.widgets.test_lineedit import find_action_from_menu
@@ -40,32 +40,39 @@ def test_construct(qtbot):
     assert pydm_spinbox.isAccelerated() is True
 
 
-@pytest.mark.parametrize("first_key_pressed, second_key_pressed, keys_pressed_expected_results", [
-    (Qt.Key_Left, Qt.Key_Right, (2, 1)),
-    (Qt.Key_Right, Qt.Key_Left, (-2, -1)),
-])
-def test_key_press_event(qtbot, signals, monkeypatch, first_key_pressed, second_key_pressed,
-                         keys_pressed_expected_results):
+@pytest.mark.parametrize(
+    "first_key_pressed, second_key_pressed, keys_pressed_expected_results", [
+        (Qt.Key_Left, Qt.Key_Right, (2, 1)),
+        (Qt.Key_Right, Qt.Key_Left, (-2, -1)),
+    ])
+def test_key_press_event(
+        qtbot, signals, monkeypatch, first_key_pressed, second_key_pressed,
+        keys_pressed_expected_results):
     """
     Test the widget's handling of the key press events.
 
     Expectations:
-    The step exponent value of the spin box will change as the Left Arrow or Right Arrow is pressed.
+    The step exponent value of the spin box will change as the Left Arrow or
+    Right Arrow is pressed.
 
     Parameters
     ----------
     qtbot : fixture
         Window for widget testing
     signals : fixture
-        The signals fixture, which provides access signals to be bound to the appropriate slots
+        The signals fixture, which provides access signals to be bound to the
+        appropriate slots
     monkeypatch : fixture
         To override default behaviors
     first_key_pressed : Qt.Key
-        The first key to press to change the spinbox's step exponent value (increase or decrease)
+        The first key to press to change the spinbox's step exponent value
+        (increase or decrease)
     second_key_pressed: Qt.Key
-        The second key to press to change the spinbox's step exponent value (increase or decrease)
+        The second key to press to change the spinbox's step exponent value
+        (increase or decrease)
     keys_pressed_expected_results : tuple
-        The new value after the first key, and then that after the second key, is pressed
+        The new value after the first key, and then that after the second key,
+        is pressed
     """
     pydm_spinbox = PyDMSpinbox()
     qtbot.addWidget(pydm_spinbox)
@@ -83,49 +90,70 @@ def test_key_press_event(qtbot, signals, monkeypatch, first_key_pressed, second_
     signals.new_value_signal[float].emit(INIT_SPINBOX_VALUE)
 
     pydm_spinbox.setFocus()
+
     def wait_focus():
         return pydm_spinbox.hasFocus()
     qtbot.waitUntil(wait_focus, timeout=5000)
 
-    def press_key_and_verify(key_pressed, key_mod, key_press_count, expected_exp, expected_value):
+    def press_key_and_verify(
+            key_pressed, key_mod, key_press_count, expected_exp,
+            expected_value):
         if key_mod != Qt.NoModifier:
-            # Monkeypatch the Control flag because in this test, we don't properly have the app QApplication instance
-            monkeypatch.setattr(QApplication, "queryKeyboardModifiers", lambda *args: key_mod)
+            # Monkeypatch the Control flag because in this test, we don't
+            # properly have the app QApplication instance
+            monkeypatch.setattr(
+                QApplication, "queryKeyboardModifiers", lambda *args: key_mod)
         else:
-            monkeypatch.setattr(QApplication, "queryKeyboardModifiers", lambda *args: Qt.NoModifier)
+            monkeypatch.setattr(
+                QApplication, "queryKeyboardModifiers",
+                lambda *args: Qt.NoModifier)
 
         for i in range(0, key_press_count):
-            pydm_spinbox.keyPressEvent(QKeyEvent(QEvent.KeyPress, key_pressed, key_mod))
+            pydm_spinbox.keyPressEvent(QKeyEvent(
+                        QEvent.KeyPress, key_pressed, key_mod))
         assert pydm_spinbox.step_exponent == expected_exp
 
         signals.send_value_signal[float].connect(signals.receiveValue)
-        signals.send_value_signal[float].connect(pydm_spinbox.channelValueChanged)
+        signals.send_value_signal[float].connect(
+                                pydm_spinbox.channelValueChanged)
 
-        # Send out the key event to update the value based on the step exponent change
-        pydm_spinbox.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_Return, Qt.NoModifier))
+        # Send out the key event to update the value based on the step
+        # exponent change
+        pydm_spinbox.keyPressEvent(QKeyEvent(
+                    QEvent.KeyPress, Qt.Key_Return, Qt.NoModifier))
         assert pydm_spinbox.value == expected_value
 
     # Send out the first key event to effect the change of the step exponent
-    press_key_and_verify(first_key_pressed, Qt.ControlModifier, 2, keys_pressed_expected_results[0],
-                         INIT_SPINBOX_VALUE)
+    press_key_and_verify(
+        first_key_pressed, Qt.ControlModifier, 2,
+        keys_pressed_expected_results[0], INIT_SPINBOX_VALUE)
 
-    # Send out the second key press event once to check if it can change the step exponent
-    press_key_and_verify(second_key_pressed, Qt.ControlModifier, 1, keys_pressed_expected_results[1],
-                         INIT_SPINBOX_VALUE)
+    # Send out the second key press event once to check if it can change the
+    # step exponent
+    press_key_and_verify(
+        second_key_pressed, Qt.ControlModifier, 1,
+        keys_pressed_expected_results[1], INIT_SPINBOX_VALUE)
 
-    # Make sure the widget can process the UpArrow, DownArrow, PageUp, and PageDown keys
-    pydm_spinbox.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_Up, Qt.ControlModifier))
-    pydm_spinbox.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_Down, Qt.ControlModifier))
-    pydm_spinbox.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_PageUp, Qt.ControlModifier))
-    pydm_spinbox.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_PageDown, Qt.ControlModifier))
+    # Make sure the widget can process the UpArrow, DownArrow, PageUp, and
+    # PageDown keys
+    pydm_spinbox.keyPressEvent(QKeyEvent(
+                QEvent.KeyPress, Qt.Key_Up, Qt.ControlModifier))
+    pydm_spinbox.keyPressEvent(QKeyEvent(
+                QEvent.KeyPress, Qt.Key_Down, Qt.ControlModifier))
+    pydm_spinbox.keyPressEvent(QKeyEvent(
+                QEvent.KeyPress, Qt.Key_PageUp, Qt.ControlModifier))
+    pydm_spinbox.keyPressEvent(QKeyEvent(
+                QEvent.KeyPress, Qt.Key_PageDown, Qt.ControlModifier))
 
 
 def test_widget_ctx_menu(qtbot):
     """
-    Test to make sure the widget's context menu contains the specific action for the widget.
+    Test to make sure the widget's context menu contains the specific action
+        for the widget.
 
     Expectations:
-    All specific actions for the widget's context menu are present in that menu.
+        All specific actions for the widget's context menu are present in that
+        menu.
 
     Parameters
     ----------
@@ -153,8 +181,9 @@ def test_update_step_size(qtbot, step_exp):
     Test the incrementing of the widget's step exponent.
 
     Expectations:
-    The widget's step exponent is incremented correctly, by verifying the value returned by the Qt API singleStep() is
-    ten times the step exponent value.
+    The widget's step exponent is incremented correctly, by verifying the
+    value returned by the Qt API singleStep() is ten times the step exponent
+    value.
 
     Parameters
     ----------
@@ -180,10 +209,12 @@ def test_update_step_size(qtbot, step_exp):
     (False, "ns", 0.01, True),
     (False, "light years", 1, False),
 ])
-def test_update_format_string(qtbot, signals, show_unit, new_unit, step_exp, show_step_exp):
+def test_update_format_string(
+        qtbot, signals, show_unit, new_unit, step_exp, show_step_exp):
     """
-    Test the widget's capability of updating the format string when various unit and step exponent paramaters are
-    updated. This method also test showStepExponent property and setter.
+    Test the widget's capability of updating the format string when various
+    unit and step exponent paramaters are updated. This method also test
+    showStepExponent property and setter.
 
     Expectations:
     The format string is correctly produced.
@@ -193,7 +224,8 @@ def test_update_format_string(qtbot, signals, show_unit, new_unit, step_exp, sho
     qtbot : fixture
         Window for widget testing
     signals : fixture
-        The signals fixture, which provides access signals to be bound to the appropriate slots
+        The signals fixture, which provides access signals to be bound to the
+        appropriate slots
     show_unit : bool
         True if the unit is to be displayed on the widget; False if otherwise
     new_unit : str
@@ -201,7 +233,8 @@ def test_update_format_string(qtbot, signals, show_unit, new_unit, step_exp, sho
     step_exp : int
         The step exponent for widget's values
     show_step_exp : bool
-        True if the step exponent is to be displayed on the widget; False if otherwise
+        True if the step exponent is to be displayed on the widget; False if
+        otherwise
     """
     pydm_spinbox = PyDMSpinbox()
     qtbot.addWidget(pydm_spinbox)
@@ -223,7 +256,8 @@ def test_update_format_string(qtbot, signals, show_unit, new_unit, step_exp, sho
 
     if show_step_exp:
         current_suffix = pydm_spinbox.suffix()
-        expected_suffix = "{0} Step: 1E{1}".format(units, pydm_spinbox.step_exponent)
+        expected_suffix = "{0} Step: 1E{1}".format(
+                            units, pydm_spinbox.step_exponent)
         assert current_suffix == expected_suffix
         assert pydm_spinbox.toolTip() == ""
     else:
@@ -243,7 +277,8 @@ def test_update_format_string(qtbot, signals, show_unit, new_unit, step_exp, sho
 ])
 def test_send_value(qtbot, signals, init_value, user_typed_value, precision):
     """
-    Test sending the value from the widget to the channel. This method tests value_changed() and precision_changed().
+    Test sending the value from the widget to the channel. This method tests
+        value_changed() and precision_changed().
 
     Expectations:
     The correct value is sent to the channelValueChanged slot.
@@ -253,7 +288,8 @@ def test_send_value(qtbot, signals, init_value, user_typed_value, precision):
     qtbot : fixture
         pytest-qt window for widget test
     signals : fixture
-        The signals fixture, which provides access signals to be bound to the appropriate slots
+        The signals fixture, which provides access signals to be bound to the
+        appropriate slots
     init_value : float
         The initial value assigned to the spinbox
     user_typed_value : float
@@ -271,13 +307,16 @@ def test_send_value(qtbot, signals, init_value, user_typed_value, precision):
     signals.prec_signal[type(precision)].emit(precision)
 
     signal_type = type(user_typed_value)
-    signals.new_value_signal[signal_type].connect(pydm_spinbox.channelValueChanged)
+    signals.new_value_signal[signal_type].connect(
+                        pydm_spinbox.channelValueChanged)
     signals.new_value_signal[signal_type].emit(user_typed_value)
 
-    # Besides receiving the new channel value, simulate the update of the new value to the widget by connecting the
-    # channelValueChanged slot to the same signal
+    # Besides receiving the new channel value, simulate the update of the new
+    # value to the widget by connecting the channelValueChanged slot to the
+    # same signal
     signals.send_value_signal[signal_type].connect(signals.receiveValue)
-    signals.send_value_signal[signal_type].connect(pydm_spinbox.channelValueChanged)
+    signals.send_value_signal[signal_type].connect(
+                                    pydm_spinbox.channelValueChanged)
     pydm_spinbox.send_value()
 
     assert pydm_spinbox.value == user_typed_value
