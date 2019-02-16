57374b8bf83db9061633d839bebe1e3dc2b16a9b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-16 01:23:42 +0000

    Update docs after building Travis build 849 of slaclab/pydm
    The docs were built from the branch 'master' against the commit
    25cea9fdf4f01ed285a40cf715e73d79cad1291d.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/slaclab/pydm/jobs/494067270.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.6.5+31.g813640f',
+    VERSION: 'v1.6.5+33.g25cea9f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
