cadf23a93be7ddd4998dc9e1b43c96b27848cac4
Azure Pipelines <azurebot@nonvalidemail.com>
2019-04-03 05:19:22 +0000

    Updating Docs with latest build

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.6.5+95.gc8f918a6',
+    VERSION: 'v1.6.5+100.gd82a6481',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
