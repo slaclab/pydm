1f1165a3609f920c074854713db16ef4f4898164
Hugo Slepicka <slepicka@slac.stanford.edu>
2019-05-10 17:45:27 -0700

    MNT: Fix Archiver plugin to new architecture.

--- pydm/data_plugins/archiver_plugin.py
+++ pydm/data_plugins/archiver_plugin.py
@@ -1,14 +1,20 @@
-from pydm.data_plugins.plugin import PyDMPlugin, PyDMConnection
 import requests
 import numpy as np
 import os
 
+from pydm.data_store import DataKeys
+from pydm.data_plugins.plugin import PyDMPlugin, PyDMConnection
+
 
 class Connection(PyDMConnection):
-
     def __init__(self, channel, address, protocol=None, parent=None):
         super(Connection, self).__init__(channel, address, protocol, parent)
-        self.add_listener(channel)
+
+        self.data[DataKeys.CONNECTION] = False
+        self.data[DataKeys.WRITE_ACCESS] = False
+        self.data[DataKeys.VALUE] = None
+        self.send_to_channel()
+
         base_url = os.getenv("PYDM_ARCHIVER_URL", "http://lcls-archapp.slac.stanford.edu")
         url_string = "{base}/retrieval/data/getData.json?{params}".format(base=base_url, params=address)
         r = requests.get(url_string)  # blocking.  BAD!
@@ -19,7 +25,8 @@ class Connection(PyDMConnection):
             # x_data not used so commented out... maybe return it with y_data?
             # x_data = np.array([point["secs"] for point in data_dict[0]["data"]])
             y_data = np.array([point["val"] for point in data_dict[0]["data"]])
-            self.new_waveform_signal.emit(y_data)
+            self.data[DataKeys.VALUE] = y_data
+            self.send_to_channel()
 
 
 class ArchiverPlugin(PyDMPlugin):
