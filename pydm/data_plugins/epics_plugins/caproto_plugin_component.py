9311c97a19ac1fd9ff12ffebd4b1e0a4840d46cc
Hugo Slepicka <slepicka@slac.stanford.edu>
2019-05-10 18:14:31 -0700

    MNT: Rework EPICS caproto layer to new architecture.

--- pydm/data_plugins/epics_plugins/caproto_plugin_component.py
+++ pydm/data_plugins/epics_plugins/caproto_plugin_component.py
@@ -1,163 +1,110 @@
-import caproto.threading.pyepics_compat as epics
-from caproto import SubscriptionType
 import logging
 import numpy as np
-from pydm.data_plugins.plugin import PyDMPlugin, PyDMConnection
 from qtpy.QtCore import Slot, Qt
 from qtpy.QtWidgets import QApplication
+
+from pydm.utilities.channel import parse_channel_config
 from pydm.data_plugins import is_read_only
+from pydm.data_store import DataKeys
+from pydm.data_plugins.plugin import PyDMPlugin, PyDMConnection
+
+import caproto.threading.pyepics_compat as epics
+from caproto import SubscriptionType
+
 
 logger = logging.getLogger(__name__)
 
-int_types = set((epics.ChannelType.INT, epics.ChannelType.CTRL_INT, epics.ChannelType.TIME_INT,
-                 epics.ChannelType.ENUM, epics.ChannelType.CTRL_ENUM, epics.ChannelType.TIME_ENUM,
-                 epics.ChannelType.TIME_LONG, epics.ChannelType.LONG, epics.ChannelType.CTRL_LONG,
-                 epics.ChannelType.CHAR, epics.ChannelType.TIME_CHAR, epics.ChannelType.CTRL_CHAR))
-
-float_types = set((epics.ChannelType.CTRL_FLOAT, epics.ChannelType.FLOAT, epics.ChannelType.TIME_FLOAT,
-                   epics.ChannelType.CTRL_DOUBLE, epics.ChannelType.DOUBLE, epics.ChannelType.TIME_DOUBLE))
-
 
 class Connection(PyDMConnection):
-
     def __init__(self, channel, pv, protocol=None, parent=None):
         super(Connection, self).__init__(channel, pv, protocol, parent)
-        self.app = QApplication.instance()
-        self._value = None
-        self._severity = None
-        self._precision = None
-        self._enum_strs = None
-        self._unit = None
-        self._upper_ctrl_limit = None
-        self._lower_ctrl_limit = None
+        conn = parse_channel_config(pv, force_dict=True)
+        address = conn.get('parameters', {}).get('address')
+        monitor_mask = SubscriptionType.DBE_VALUE
+        monitor_mask |= SubscriptionType.DBE_ALARM
+        monitor_mask |= SubscriptionType.DBE_PROPERTY
 
-        self.pv = epics.get_pv(pv, connection_callback=self.send_connection_state, form='ctrl', auto_monitor=SubscriptionType.DBE_VALUE|SubscriptionType.DBE_ALARM|SubscriptionType.DBE_PROPERTY, access_callback=self.send_access_state)
+        self.pv = epics.get_pv(address, form='ctrl', auto_monitor=monitor_mask,
+                               access_callback=self.send_access_state,
+                               connection_callback=self.send_connection_state)
         self.pv.add_callback(self.send_new_value, with_ctrlvars=True)
-        self.add_listener(channel)
 
-    def clear_cache(self):
-        self._value = None
-        self._severity = None
-        self._precision = None
-        self._enum_strs = None
-        self._unit = None
-        self._upper_ctrl_limit = None
-        self._lower_ctrl_limit = None
-
-    def send_new_value(self, value=None, char_value=None, count=None, typefull=None, type=None, *args, **kws):
+    def send_new_value(self, value=None, **kws):
         self.update_ctrl_vars(**kws)
 
-        if value is not None and not np.array_equal(value, self._value):
-            self._value = value
-            if isinstance(value, np.ndarray):
-                self.new_value_signal[np.ndarray].emit(value)
-            else:
-                if typefull in int_types:
-                    try:
-                        self.new_value_signal[int].emit(int(value))
-                    except ValueError:  # This happens when a string is empty
-                        # HACK since looks like for PyEpics a 1 element array
-                        # is in fact a scalar. =( I will try to address this
-                        # with Matt Newville
-                        self.new_value_signal[str].emit(char_value)
-                elif typefull in float_types:
-                    self.new_value_signal[float].emit(float(value))
-                else:
-                    self.new_value_signal[str].emit(char_value)
+        if value is not None:
+            self.data[DataKeys.VALUE] = value
 
-    def update_ctrl_vars(self, units=None, enum_strs=None, severity=None, upper_ctrl_limit=None, lower_ctrl_limit=None, precision=None, *args, **kws):
-        if severity is not None and self._severity != severity:
-            self._severity = severity
-            self.new_severity_signal.emit(int(severity))
-        if precision is not None and self._precision != precision:
-            self._precision = precision
-            self.prec_signal.emit(precision)
-        if enum_strs is not None and self._enum_strs != enum_strs:
-            self._enum_strs = enum_strs
+        self.send_to_channel()
+
+    def update_ctrl_vars(self, units=None, enum_strs=None, severity=None,
+                         upper_ctrl_limit=None, lower_ctrl_limit=None,
+                         precision=None, *args, **kws):
+        if severity is not None:
+            self.data[DataKeys.SEVERITY] = severity
+        if precision is not None:
+            self.data[DataKeys.PRECISION] = precision
+        if enum_strs is not None:
             try:
-                enum_strs = tuple(b.decode(encoding='ascii') for b in enum_strs)
+                enum_strs = tuple(
+                    b.decode(encoding='ascii') for b in enum_strs)
             except AttributeError:
                 pass
-            self.enum_strings_signal.emit(enum_strs)
-        if units is not None and len(units) > 0 and self._unit != units:
+            self.data[DataKeys.ENUM_STRINGS] = enum_strs
+        if units is not None and len(units) > 0:
             if type(units) == bytes:
                 units = units.decode()
-            self._unit = units
-            self.unit_signal.emit(units)
-        if upper_ctrl_limit is not None and self._upper_ctrl_limit != upper_ctrl_limit:
-            self._upper_ctrl_limit = upper_ctrl_limit
-            self.upper_ctrl_limit_signal.emit(upper_ctrl_limit)
-        if lower_ctrl_limit is not None and self._lower_ctrl_limit != lower_ctrl_limit:
-            self._lower_ctrl_limit = lower_ctrl_limit
-            self.lower_ctrl_limit_signal.emit(lower_ctrl_limit)
+            self.data[DataKeys.UNIT] = units
+        if upper_ctrl_limit is not None:
+            self.data[DataKeys.UPPER_LIMIT] = upper_ctrl_limit
+        if lower_ctrl_limit is not None:
+            self.data[DataKeys.LOWER_LIMIT] = lower_ctrl_limit
 
     def send_access_state(self, read_access, write_access, *args, **kws):
         if is_read_only():
-            self.write_access_signal.emit(False)
+            self.data[DataKeys.WRITE_ACCESS] = False
             return
 
         if write_access is not None:
-            self.write_access_signal.emit(write_access)
+            self.data[DataKeys.WRITE_ACCESS] = write_access
+        self.send_to_channel()
 
     def reload_access_state(self):
-        read_access = self.pv.read_access
-        write_access = self.pv.write_access
+        read_access = epics.ca.read_access(self.pv.chid)
+        write_access = epics.ca.write_access(self.pv.chid)
         self.send_access_state(read_access, write_access)
 
     def send_connection_state(self, conn=None, *args, **kws):
-        self.connected = conn
-        self.connection_state_signal.emit(conn)
+        self.data[DataKeys.CONNECTION] = conn
         if conn:
-            self.clear_cache()
             if hasattr(self, 'pv'):
                 self.reload_access_state()
                 self.pv.run_callbacks()
+        self.send_to_channel()
 
-    @Slot(int)
-    @Slot(float)
-    @Slot(str)
-    @Slot(np.ndarray)
-    def put_value(self, new_val):
-        if is_read_only():
+    @Slot(dict)
+    def receive_from_channel(self, payload):
+        new_val = payload.get(DataKeys.VALUE, None)
+        if new_val is None:
+            logger.warning("Could not write to PV. No new value for key %s."
+                           "PV: %s",
+                           DataKeys.VALUE, self.pv.pvname)
             return
 
-        if self.pv.write_access:
-            try:
-                self.pv.put(new_val)
-            except Exception as e:
-                logger.exception("Unable to put %s to %s.  Exception: %s",
-                                 new_val, self.pv.pvname, str(e))
+        if not self.pv.write_access:
+            logger.warning("Could not write to PV. Write access denied for "
+                           "PV: %s", self.pv.pvname)
+            return
 
-    def add_listener(self, channel):
-        super(Connection, self).add_listener(channel)
-        # If we are adding a listener to an already existing PV, we need to
-        # manually send the signals indicating that the PV is connected, what the latest value is, etc.
-        if self.pv.connected:
-            self.send_connection_state(conn=True)
-            self.pv.run_callbacks()
-        else:
-            self.send_connection_state(conn=False)
-        # If the channel is used for writing to PVs, hook it up to the 'put' methods.
-        if channel.value_signal is not None:
-            try:
-                channel.value_signal[str].connect(self.put_value, Qt.QueuedConnection)
-            except KeyError:
-                pass
-            try:
-                channel.value_signal[int].connect(self.put_value, Qt.QueuedConnection)
-            except KeyError:
-                pass
-            try:
-                channel.value_signal[float].connect(self.put_value, Qt.QueuedConnection)
-            except KeyError:
-                pass
-            try:
-                channel.value_signal[np.ndarray].connect(self.put_value, Qt.QueuedConnection)
-            except KeyError:
-                pass
+        try:
+            self.pv.put(new_val)
+        except Exception as e:
+            logger.exception("Unable to put %s to %s.  Exception: %s",
+                             new_val, self.pv.pvname, str(e))
 
     def close(self):
         self.pv.disconnect()
+        super(Connection, self).close()
 
 
 class CaprotoPlugin(PyDMPlugin):
