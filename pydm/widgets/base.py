41c3fa690f777e49c0bd1a14e367e719c650ed8d
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-06-09 11:39:14 +0200

    FIX: Use base type as channeltype when incoming value is an instance of scalar subclass

--- pydm/widgets/base.py
+++ pydm/widgets/base.py
@@ -680,6 +680,12 @@ class PyDMWidget(PyDMPrimitiveWidget):
         """
         self.value = new_val
         self.channeltype = type(self.value)
+        for base_type in [int, float, str, np.ndarray]:
+            if self.channeltype != base_type and issubclass(self.channeltype, base_type):
+                # Leave bool and int separate, we're concerned about user-defined subclasses, not native Python ones
+                if self.channeltype != bool:
+                    self.channeltype = base_type
+                break
         if self.channeltype == np.ndarray:
             self.subtype = self.value.dtype.type
         else:
