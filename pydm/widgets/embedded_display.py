3cc0927e84ad89e97819e3911b275a77c5049681
YektaY <yektay1@gmail.com>
2021-10-01 14:06:51 -0700

    added the ability to add a 'Filename' rule property for the embedded display widget

--- pydm/widgets/embedded_display.py
+++ pydm/widgets/embedded_display.py
@@ -27,6 +27,10 @@ class PyDMEmbeddedDisplay(QFrame, PyDMPrimitiveWidget):
     def __init__(self, parent=None):
         QFrame.__init__(self, parent)
         PyDMPrimitiveWidget.__init__(self)
+        if 'Filename' not in PyDMEmbeddedDisplay.RULE_PROPERTIES:
+            PyDMEmbeddedDisplay.RULE_PROPERTIES = PyDMPrimitiveWidget.RULE_PROPERTIES.copy()
+            PyDMEmbeddedDisplay.RULE_PROPERTIES.update(
+                {'Filename': ['value_changed', str]})
         self.app = QApplication.instance()
         self._filename = None
         self._macros = None
