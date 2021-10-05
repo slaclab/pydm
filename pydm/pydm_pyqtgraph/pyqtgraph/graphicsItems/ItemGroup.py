5b0da39f6936662567131185e9f0ef3a2a202024
Jesse Bellister <jbellister@debian-desktop>
2021-10-04 23:54:46 -0700

    Add the pyqtgraph source code into the project

---
+++ pydm/pydm_pyqtgraph/pyqtgraph/graphicsItems/ItemGroup.py
@@ -0,0 +1,22 @@
+from ..Qt import QtGui, QtCore
+from .GraphicsObject import GraphicsObject
+
+__all__ = ['ItemGroup']
+class ItemGroup(GraphicsObject):
+    """
+    Replacement for QGraphicsItemGroup
+    """
+    
+    def __init__(self, *args):
+        GraphicsObject.__init__(self, *args)
+        self.setFlag(self.GraphicsItemFlag.ItemHasNoContents)
+    
+    def boundingRect(self):
+        return QtCore.QRectF()
+        
+    def paint(self, *args):
+        pass
+    
+    def addItem(self, item):
+        item.setParentItem(self)
+
---
+++ pydm/pydm_pyqtgraph/pyqtgraph/widgets/BusyCursor.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from contextlib import contextmanager
+
+from ..Qt import QtGui, QtCore
+
+__all__ = ["BusyCursor"]
+
+
+@contextmanager
+def BusyCursor():
+    """
+    Display a busy mouse cursor during long operations.
+    Usage::
+
+        with BusyCursor():
+            doLongOperation()
+
+    May be nested. If called from a non-gui thread, then the cursor will not be affected.
+    """
+    app = QtCore.QCoreApplication.instance()
+    in_gui_thread = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
+    try:
+        if in_gui_thread:
+            QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CursorShape.WaitCursor))
+        yield
+    finally:
+        if in_gui_thread:
+            QtGui.QApplication.restoreOverrideCursor()
