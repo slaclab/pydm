60c7b732219a869c9133272fc24114bb9df05723
jbellister-slac <jesseb@slac.stanford.edu>
2021-10-05 12:56:13 -0700

    Exclude the (quite thorough) pyqtgraph test suite from running by default to cut down on added dependencies and time

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,8 @@ import pytest
 if __name__ == '__main__':
     # Show output results from every test function
     # Show the message output for skipped and expected failures
-    # Ignore the pyqtgraph test suite by default, can be run separately if working on pyqtgraph code
+    # Ignore the pyqtgraph test suite by default, can be run separately if working on pyqtgraph code. These tests are
+    # located at pydm/pydm_pyqtgraph/examples and pydm/pydm_pyqtgraph/examples
     args = ['-v', '-vrxs', '--ignore=pydm/pydm_pyqtgraph/']
 
     # Add extra arguments
