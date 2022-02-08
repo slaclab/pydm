2045f5f3093062a80b64e0d357c394720e633a6d
Azure Pipelines <vsts@fv-az197-468.ifdd3o2d4tsuffec2lhi3rwtte.gx.internal.cloudapp.net>
2022-02-08 18:32:28 +0000

    Updating Docs with latest build

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.12.0+8.gc157c740',
+    VERSION: 'v1.12.0+24.gd6036caf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
