dc2783a31c36b57f7bd6cd0431ffca2bc1fe1be1
Ken Lauer <klauer@slac.stanford.edu>
2022-02-10 09:21:26 -0800

    ENH: channel, filename, and macros

--- pydm/widgets/designer_settings.py
+++ pydm/widgets/designer_settings.py
@@ -1,19 +1,101 @@
 import os
 import json
+import logging
 import functools
 import webbrowser
 
 from qtpy import QtWidgets, QtCore, QtDesigner
 from ..utilities.iconfont import IconFont
+from ..utilities.macro import parse_macro_string
+from ..utilities import copy_to_clipboard, get_clipboard_text
+
+
+logger = logging.getLogger(__name__)
 
 
 def update_property_for_widget(widget: QtWidgets.QWidget, name: str, value):
     """Update a Property for the given widget in the designer."""
     formWindow = QtDesigner.QDesignerFormWindowInterface.findFormWindow(widget)
+    logger.info("Updating %s.%s = %s", widget.objectName(), name, value)
     if formWindow:
         formWindow.cursor().setProperty(name, value)
     else:
         setattr(widget, name, value)
+
+
+class DictionaryTable(QtWidgets.QTableWidget):
+    def __init__(self, dct=None, parent=None):
+        super().__init__(parent=parent)
+
+        self.setColumnCount(2)
+        self.setMinimumSize(300, 200)
+        self.setHorizontalHeaderLabels(["Key", "Value"])
+
+        self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self._context_menu)
+        self.dictionary = dct
+
+    def _context_menu(self, pos):
+        self.menu = QtWidgets.QMenu(self)
+        item = self.itemAt(pos)
+        if item is not None:
+            def copy(*_):
+                copy_to_clipboard(item.text())
+
+            copy_action = self.menu.addAction(f"&Copy: {item.text()}")
+            copy_action.triggered.connect(copy)
+
+            clipboard_text = get_clipboard_text()
+
+            def paste(*_):
+                item.setText(clipboard_text)
+
+            paste_action = self.menu.addAction(f"&Paste: {clipboard_text}")
+            paste_action.triggered.connect(paste)
+
+            def delete_row(*_):
+                self.removeRow(item.row())
+
+            delete_row_action = self.menu.addAction("&Delete row...")
+            delete_row_action.triggered.connect(delete_row)
+
+        self.menu.addSeparator()
+
+        def add_row(*_):
+            row = self.rowCount()
+            self.setRowCount(row + 1)
+            self.setItem(row, 0, QtWidgets.QTableWidgetItem(""))
+            self.setItem(row, 1, QtWidgets.QTableWidgetItem(""))
+
+        add_row_action = self.menu.addAction("&Add row...")
+        add_row_action.triggered.connect(add_row)
+        self.menu.exec_(self.mapToGlobal(pos))
+
+    @property
+    def dictionary(self) -> dict:
+        items = [
+            (self.item(row, 0), self.item(row, 1))
+            for row in range(self.rowCount())
+        ]
+        key_value_pairs = [
+            (key.text() if key else "", value.text() if value else "")
+            for key, value in items
+        ]
+        return {
+            key.strip(): value
+            for key, value in key_value_pairs
+        }
+
+
+    @dictionary.setter
+    def dictionary(self, dct):
+        self.setRowCount(len(dct))
+        for row, (key, value) in enumerate(dct.items()):
+            self.setItem(row, 0, QtWidgets.QTableWidgetItem(key))
+            self.setItem(row, 1, QtWidgets.QTableWidgetItem(value))
+
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents()
 
 
 class BasicSettingsEditor(QtWidgets.QDialog):
@@ -35,12 +117,12 @@ class BasicSettingsEditor(QtWidgets.QDialog):
         self.setMinimumSize(400, 200)
 
         self.setSizePolicy(
-            QtWidgets.QSizePolicy.MinimumExpanding, 
-            QtWidgets.QSizePolicy.MinimumExpanding, 
+            QtWidgets.QSizePolicy.MinimumExpanding,
+            QtWidgets.QSizePolicy.MinimumExpanding,
         )
 
         self.setup_ui()
-        
+
     def setup_ui(self):
         """
         Create the required UI elements for the form.
@@ -57,27 +139,42 @@ class BasicSettingsEditor(QtWidgets.QDialog):
         vlayout.setSpacing(5)
         self.setLayout(vlayout)
 
+        settings_form = QtWidgets.QFormLayout()
+        vlayout.addLayout(settings_form)
+
         if not hasattr(self.widget, "channel"):
             self.channel_widget = None
         else:
-            channel_form = QtWidgets.QFormLayout()
             self.channel_widget = QtWidgets.QLineEdit(
                 self.widget.channel or ""
             )
-            channel_form.addRow("&Channel", self.channel_widget)
-            vlayout.addLayout(channel_form)
+            settings_form.addRow("&Channel", self.channel_widget)
+
+        if not hasattr(self.widget, "channel"):
+            self.channel_widget = None
+        else:
+            self.channel_widget = QtWidgets.QLineEdit(
+                self.widget.channel or ""
+            )
+            settings_form.addRow("&Channel", self.channel_widget)
+
+        if not hasattr(self.widget, "filename"):
+            self.filename_widget = None
+        else:
+            self.filename_widget = QtWidgets.QLineEdit(
+                self.widget.filename or ""
+            )
+            settings_form.addRow("&Filename", self.filename_widget)
 
         if not hasattr(self.widget, "macros"):
             self.macros_widget = None
         else:
             # Ideally macros wouldn't be shown as a line edit; consider a table
             # or something easy to edit and interpret
-            macros_form = QtWidgets.QFormLayout()
-            self.macros_widget = QtWidgets.QLineEdit(
-                self.widget.macros or ""
+            self.macros_widget = DictionaryTable(
+                parse_macro_string(self.widget.macros or "")
             )
-            macros_form.addRow("&Macros", self.macros_widget)
-            vlayout.addLayout(macros_form)
+            settings_form.addRow("&Macros", self.macros_widget)
 
         def open_rules_editor():
             from .rules_editor import RulesEditor
@@ -107,9 +204,14 @@ class BasicSettingsEditor(QtWidgets.QDialog):
     def save_changes(self):
         """Save the new settings on the widget properties."""
         if self.channel_widget is not None:
-            update_property_for_widget(self.widget, "channel", self.channel_widget.text() or "")
+            channel = (self.channel_widget.text() or "").strip()
+            update_property_for_widget(self.widget, "channel", channel)
         if self.macros_widget is not None:
-            update_property_for_widget(self.widget, "macros", self.macros_widget.text() or "")
+            macros = json.dumps(self.macros_widget.dictionary)
+            update_property_for_widget(self.widget, "macros", macros)
+        if self.filename_widget is not None:
+            filename = self.filename_widget.text().strip()
+            update_property_for_widget(self.widget, "filename", filename)
         self.accept()
 
     @QtCore.Slot()
