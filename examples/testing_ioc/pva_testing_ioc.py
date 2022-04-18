0fb94a16ca246d297c31653706a343f047f2dded
jbellister-slac <jesseb@slac.stanford.edu>
2022-04-18 15:48:35 -0700

    Add files I forgot

--- examples/testing_ioc/pva_testing_ioc.py
+++ examples/testing_ioc/pva_testing_ioc.py
@@ -1,8 +1,10 @@
 from p4p.client.thread import Context, Disconnected
-from p4p.nt import NTScalar
+from p4p.nt import NTNDArray, NTScalar
 from p4p.server import Server, ServerOperation
 from p4p.server.thread import SharedPV
 from p4p.wrapper import Value
+import blosc
+import numpy as np
 import random
 import threading
 import time
@@ -11,44 +13,69 @@ from collections import OrderedDict
 
 class Handler(object):
 
-    def put(self, pv, op: ServerOperation):
-        """
-        Called each time a client issues a Put
-        operation on this Channel.
-
-        :param SharedPV pv: The :py:class:`SharedPV` which this Handler is associated with.
-        :param ServerOperation op: The operation being initiated.
-        """
-        pv.post(op.value(), timestamp=time.time())  # Note p4p > 4.0 only
+    def put(self, pv: SharedPV, op: ServerOperation):
+        """ Called each time a client issues a put operation on the channel using this handler """
+        pv.post(op.value())
+#        pv.post(op.value(), timestamp=time.time())  # Note timestamp is valid for p4p > 4.0 only
         op.done()
 
 
 class PVServer(object):
-
+    """ A simple server created by p4p for making some PVs to read and write test data to """
     def __init__(self):
         self.create_pvs()
-        self.ctxt = Context('pva', nt=False)
+        self.context = Context('pva', nt=False)
         self.event = threading.Event()
-        self.sim_thread = threading.Thread(target=self.run_simulation)
+        self.sim_thread = threading.Thread(target=self.update_pvs)
         self.sim_thread.setDaemon(True)
         self.sim_thread.start()
         print('Creating PV server')
         self.server_thread = threading.Thread(target=self.run_server)
         self.server_thread.setDaemon(True)
         self.server_thread.start()
-        #Server.forever(providers=[{'PyDM:TEST:MeanValue': self.mean_value, }])  # runs until KeyboardInterrupt
 
     def create_pvs(self):
-        handler = Handler()
-        self.mean_value = SharedPV(handler=handler, nt=NTScalar('d', display=True, control=True, valueAlarm=True), initial=0.0)
+        """ Create a few PVs for interacting with """
+        handler = Handler()  # A simple default handler that will just post is sufficient here
+        nt = NTScalar("i", display=True, control=True, valueAlarm=True)
+        initial = nt.wrap({'value': 5, 'valueAlarm': {'lowAlarmLimit': 2, 'lowWarningLimit': 3, 'highAlarmLimit': 9, 'highWarningLimit': 8}})
+
+        # A simple NTScalar that holds an int
+        self.mean_value = SharedPV(handler=handler, nt=NTScalar('i', display=True, control=True, valueAlarm=True), initial=initial)
+
+        # An NTNDArray that will be used to hold image data
+        self.image_pv = SharedPV(handler=handler, nt=NTNDArray(), initial=np.zeros(1))
 
     def run_server(self):
-        Server.forever(providers=[{'PyDM:TEST:MeanValue': self.mean_value, }])  # runs until KeyboardInterrupt
+        """ Run the server that will serve the PVs until keyboard interrupt """
+        Server.forever(providers=[{'PyDM:TEST:MeanValue': self.mean_value, 'PyDM:TEST:Image': self.image_pv }])
 
-    def run_simulation(self):
+    def gaussian_2d(self, x, y, x0, y0, xsig, ysig):
+        return np.exp(-0.5 * (((x - x0) / xsig) ** 2 + ((y - y0) / ysig) ** 2))
+
+    def update_pvs(self):
+        """ Constantly update the values of our PVs to simulate an actual environment """
         while True:
-            self.event.wait(1)
-            self.ctxt.put('PyDM:TEST:MeanValue', random.randint(1, 10))
+            self.event.wait(0.3)
+#            self.ctxt.put('PyDM:TEST:MeanValue', random.randint(1, 100))
+#            self.ctxt.put('jesseb:temperature', random.randint(0, 150))
+            x = np.linspace(-5.0, 5.0, 512)
+            y = np.linspace(-5.0, 5.0, 512)
+            x0 = 0.5 * (np.random.rand() - 0.5)
+            y0 = 0.5 * (np.random.rand() - 0.5)
+            xsig = 0.8 - 0.2 * np.random.rand()
+            ysig = 0.8 - 0.2 * np.random.rand()
+            xgrid, ygrid = np.meshgrid(x, y)
+            z = self.gaussian_2d(xgrid, ygrid, x0, y0, xsig, ysig)
+            image_data = np.abs(256.0 * (z)).flatten(order='C').astype(np.uint8, copy=False)
+            view = memoryview(image_data)
+#            compressed_data = blosc.compress(view, typesize=8)
+            self.context.put('PyDM:TEST:Image', {'value': image_data, 'dimension': [{'size': 512, 'offset': 0}, {'size': 512, 'offset': 0}]})
+            self.context.put('PyDM:TEST:MeanValue', random.randint(1, 100))
+#            self.context.put('PyDM:TEST:Image',
+#                             {'value': image_data, 'dimension': [{'size': 512, 'offset': 0}, {'size': 512, 'offset': 0}],
+#                              'codec': {'name': 'blosc', 'parameters': 1}})
+
 
 
 class PVClient(object):
@@ -57,31 +84,57 @@ class PVClient(object):
         self.cli_ctxt = Context('pva', nt=False)
 
     def monitor_pvs(self):
-        self.monitor = self.cli_ctxt.monitor(name='jesseb:temperature', cb=self.send_new_value, notify_disconnect=True)
+        self.monitor = self.cli_ctxt.monitor(name='PyDM:TEST:Image', cb=self.send_new_value, notify_disconnect=True)
+        self.event = threading.Event()
+        #self.monitor = self.cli_ctxt.monitor(name='jesseb:temperature', cb=self.send_new_value, notify_disconnect=True)
+        #self.monitor = self.cli_ctxt.monitor(name='PyDM:TEST:MeanValue', cb=self.send_new_value, notify_disconnect=True)
+
+    def run_simulation(self):
+        while True:
+            self.event.wait(1.5)
+            result = self.cli_ctxt.get(name='pv:face')
+            data = result.value.copy()
+            #print(type(data))
+            #print(result.value.shape)
+            data += 40
+            data[data > 255] %= 255
+            nt = NTNDArray()
+            #self.cli_ctxt.put('pv:face', {'value': data})
+            #value = nt.wrap({'value': data, 'dimension': [{'size': 1024, 'offset': 0, 'fullSize': 1024}, {'size': 768, 'offset':0, 'fullsize': 768}]})
+            #self.cli_ctxt.put('pv:face', value)
+            self.cli_ctxt.put('pv:face', {'value': data, 'dimension': [{'size': 1024, 'offset': 0}, {'size': 768, 'offset': 0}]})
 
     def send_new_value(self, value: Value):
         if isinstance(value, Disconnected):
             print('Disconnected')
             return
-        print(value)
-        print(value.value)
-        print(value.alarm.severity)
-        print('\n\n\n\n\n')
-        print(value.todict(None, OrderedDict))
-        print('\n\n\n\n\n')
-        print(value.asSet())
-        print(value.changed('value'))
-        print(value.changed('alarm'))
-        print(value.changed('alarm.severity'))
-        print(value.changedSet(expand=False, parents=False))
+        print(value.changedSet())
+#        print(value.dimension)
+#        print(value.getID())
+#        print(value)
+#        print(value.dimension)
+#        print(value.dimension[0].size)
+        #print(value.value.shape)
+#        print(value)
+#        print(value.value)
+#        print(f'\n TYPE IS: {type(value.value)}\n')
+#        print(value.alarm.severity)
+#        print('\n\n\n\n\n')
+#        print(value.todict(None, OrderedDict))
+#        print('\n\n\n\n\n')
+#        print(value.asSet())
+##        print(value.changed('value'))
+ #       print(value.changed('alarm'))
+ #       print(value.changed('alarm.severity'))
 
 
 if __name__ == '__main__':
     try:
         print('Starting testing-ioc')
-        #server = PVServer()
-        client = PVClient()
-        client.monitor_pvs()
+        server = PVServer()
+#        client = PVClient()
+#        client.monitor_pvs()
+        #client.run_simulation()
         while True:
             pass
     except KeyboardInterrupt:
--- pydm/data_plugins/epics_plugins/p4p_plugin_component.py
+++ pydm/data_plugins/epics_plugins/p4p_plugin_component.py
@@ -3,22 +3,25 @@ import numpy as np
 
 from p4p.client.thread import Context, Disconnected
 from p4p.wrapper import Value
-from qtpy.QtCore import Slot
+from qtpy.QtCore import Slot, Qt
+from pydm.data_plugins import is_read_only
 from pydm.data_plugins.plugin import PyDMPlugin, PyDMConnection
+from typing import Set
+from .pva_codec import decompress
 
 
 logger = logging.getLogger(__name__)
 
 
 class PVAContext(object):
-    """ Singleton class responsible for holding the pva context. """
+    """ Singleton class responsible for holding the p4p pva context. """
     __instance = None
 
     def __init__(self):
         if self.__initialized:
             return
         self.__initialized = True
-        self.context = Context('pva', maxsize=2)
+        self.context = Context('pva', maxsize=2, nt=False)
 
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
@@ -35,11 +38,12 @@ class Connection(PyDMConnection):
         self.monitor = PVAContext().context.monitor(name=address,
                                                     cb=self.send_new_value,
                                                     notify_disconnect=True)
+        self.add_listener(channel)
         self._value = None
         self._severity = None
         self._precision = None
         self._enum_strs = None
-        self._unit = None
+        self._units = None
         self._upper_ctrl_limit = None
         self._lower_ctrl_limit = None
 
@@ -48,27 +52,111 @@ class Connection(PyDMConnection):
         self._severity = None
         self._precision = None
         self._enum_strs = None
-        self._unit = None
+        self._units = None
         self._upper_ctrl_limit = None
         self._lower_ctrl_limit = None
+
+#{'valueAlarm.active', 'valueAlarm.highWarningLimit', 'control.limitHigh', 'value', 'valueAlarm.lowAlarmSeverity', 'valueAlarm.lowWarningSeverity', 'alarm.message',
+    # 'display.limitLow', 'display.limitHigh', 'valueAlarm.hysteresis', 'valueAlarm.highAlarmSeverity', 'alarm', 'alarm.severity', 'timeStamp.nanoseconds', 'control.limitLow',
+    # 'timeStamp.secondsPastEpoch', 'display.form.choices', 'valueAlarm.highWarningSeverity', 'control', 'timeStamp.userTag', 'timeStamp', 'display.form', 'control.minStep',
+    # 'display', 'display.form.index', 'alarm.status', 'valueAlarm.highAlarmLimit', 'display.precision', 'valueAlarm.lowAlarmLimit', 'valueAlarm.lowWarningLimit', 'display.description',
+    # 'display.units', 'valueAlarm'}
+
+
 
     def send_new_value(self, value: Value):
         if isinstance(value, Disconnected):
             self._connected = False
+            self.clear_cache()
             self.connection_state_signal.emit(False)
-
-            self.data = {'CONNECTION': False}
         else:
             if not self._connected:
                 self._connected = True
                 self.connection_state_signal.emit(True)
-                # TODO: It sure would be nice if the changedSet worked the way I expect it to
-            if value.value is not None and not np.array_equal(value, self._value):
-                self.
+
+            self.write_access_signal.emit(True)  # TODO: This should probably come from somewhere?
+
+            for changed_value in value.changedSet():
+                if changed_value == 'value' and not np.array_equal(value.value, self._value):
+                    if 'NTNDArray' in value.getID():
+                        self.preprocess_array(value)
+                    new_value = value.value
+                    if new_value is not None and not np.array_equal(new_value, self._value):
+                        self._value = new_value
+                        if isinstance(new_value, np.ndarray):
+                            self.new_value_signal[np.ndarray].emit(new_value)
+                        elif isinstance(new_value, float):
+                            self.new_value_signal[float].emit(new_value)
+                        elif isinstance(new_value, int):
+                            self.new_value_signal[int].emit(new_value)
+                        elif isinstance(new_value, str):
+                            self.new_value_signal[str].emit(new_value)
+                        else:
+                            raise ValueError(f'No matching signal for value: {value} with type: {type(value)}')
+                elif changed_value == 'alarm.severity' and value.alarm.severity != self._severity:
+                    self._severity = value.alarm.severity
+                    print(f'sending new severity: {value.alarm.severity}')
+                    self.new_severity_signal.emit(value.alarm.severity)
+                elif changed_value == 'display.precision' and value.display.precision != self._precision:
+                    self._precision = value.display.precision
+                    self.prec_signal.emit(value.display.precision)
+ #               elif changed_value == 'display.form.choices' and value.display.form.choices != self._enum_strs: # TODO: Is this actually the same thing?
+ #                   self._enum_strs = value.display.form.choices
+ #                   self.enum_strings_signal.emit(value.display.form.choices)
+                elif changed_value == 'display.units' and value.display.units != self._units:
+                    self._units = value.display.units
+                    self.unit_signal.emit(value.display.units)
+                elif changed_value == 'control.limitLow' and value.control.limitLow != self._lower_ctrl_limit:
+                    self._lower_ctrl_limit = value.control.limitLow
+                    self.lower_ctrl_limit_signal.emit(value.control.limitLow)
+                elif changed_value == 'control.limitHigh' and value.control.limitHigh != self._upper_ctrl_limit:
+                    self._upper_ctrl_limit = value.control.limitHigh
+                    self.upper_ctrl_limit_signal.emit(value.control.limitHigh)
+
+    def preprocess_array(self, value):
+        decompress(value)
+#        try:
+#            decompress(value)
+#        except Exception as e:
+#            print(f'Could not decompress data: exception: {e}')
+
+
+    def put_value(self, value):
+        if is_read_only():
+            return
+
+        # Check write access?
+        try:
+            PVAContext().context.put(self.monitor.name, value)
+        except Exception as e:
+            logger.exception(f"Unable to put value: {value} to channel: {self.monitor.name}  Exception: {e}")
+
+    def add_listener(self, channel):
+        print(f'Adding listener for channel: {channel}')
+        super().add_listener(channel)
+
+        if channel.value_signal is not None:
+            try:
+                channel.value_signal[str].connect(self.put_value, Qt.QueuedConnection)
+            except KeyError:
+                pass
+            try:
+                channel.value_signal[int].connect(self.put_value, Qt.QueuedConnection)
+            except KeyError:
+                pass
+            try:
+                channel.value_signal[float].connect(self.put_value, Qt.QueuedConnection)
+            except KeyError:
+                pass
+            try:
+                channel.value_signal[np.ndarray].connect(self.put_value, Qt.QueuedConnection)
+            except KeyError:
+                pass
 
     def close(self):
         self.monitor.close()
-        super(Connection, self).close()
+        # TODO: Anything else?
+        super().close()
 
 
 class P4PPlugin(PyDMPlugin):
