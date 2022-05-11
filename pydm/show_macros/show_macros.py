8858d53f37893e09e1c41d0391ee7d096af640fd
Kyle Leleux <kleleux@slac.stanford.edu>
2022-05-11 15:32:42 -0700

    Added QPushButton that copies macros to clipboard as JSON object with text that notifies you when you copied to clipboard.

--- pydm/show_macros/show_macros.py
+++ pydm/show_macros/show_macros.py
@@ -1,7 +1,9 @@
-from qtpy.QtWidgets import QWidget, QPlainTextEdit, QVBoxLayout
+from qtpy.QtWidgets import QWidget, QPlainTextEdit, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QApplication, QLabel
 from qtpy.QtCore import Qt
 from qtpy.QtGui import QFont
+#from qtpy.QtGui
 
+#clipboard = QGuiApplication.clipboard()
 
 class MacroWindow(QWidget):
     """
@@ -13,21 +15,50 @@ class MacroWindow(QWidget):
         self.setFixedWidth(750)
         self.setFixedHeight(500)
 
-        text_box = QPlainTextEdit()
-        text_box.setReadOnly(True)
-        font = QFont()
-        font.setPointSize(16)
-        text_box.setFont(font)
-        text_box.setPlainText("Macros:")
+        self.text_box = QPlainTextEdit()
+        self.text_box.setReadOnly(True)
+        self.font = QFont()
+        self.font.setPointSize(16)
+        self.text_box.setFont(self.font)
+        self.text_box.setPlainText("Macros:")
 
-        # Populate macros
-        if parent is not None:
-            display_widget = parent.display_widget()
-            if display_widget:
-                macros = display_widget.macros()
-                for macro, value in macros.items():
-                    line = "  {}={}".format(macro, value)
-                    text_box.appendPlainText(line)
+        self.macros = {}
+        self.init_macros()
+        self.populate_macros()
 
-        self.setLayout(QVBoxLayout(self))
-        self.layout().addWidget(text_box)
+        self.label = QLabel()
+        self.label.setText('')
+        self.label.setMinimumSize(100, 25)
+
+        self.copy_button = QPushButton()
+        self.copy_button.setCheckable(False)
+        self.copy_button.setMinimumSize(75, 25)
+        self.copy_button.setText("Copy Macros as JSON")
+        self.copy_button.clicked.connect(self.copy_macros)
+
+        self.clipboard = QApplication.clipboard()
+
+        self.vBoxLayout = QVBoxLayout()
+        self.vBoxLayout.addWidget(self.text_box)
+        self.hBoxLayout = QHBoxLayout()
+        self.hBoxLayout.addWidget(self.label, alignment=Qt.AlignRight)
+        self.hBoxLayout.addWidget(self.copy_button, alignment=Qt.AlignRight)
+        self.hBoxLayout.insertStretch(0)
+        self.vBoxLayout.addLayout(self.hBoxLayout)
+
+        self.setLayout(self.vBoxLayout)
+
+    def init_macros(self):
+        if self.parent() is not None:
+            display_widget = self.parent().display_widget()
+            if display_widget is not None:
+                self.macros = display_widget.macros()
+
+    def populate_macros(self):
+        for macro, value in self.macros.items():
+            line = "  {}={}".format(macro, value)
+            self.text_box.appendPlainText(line)
+
+    def copy_macros(self):
+        self.clipboard.setText(str(self.macros))
+        self.label.setText('Macros copied to clipboard!')
