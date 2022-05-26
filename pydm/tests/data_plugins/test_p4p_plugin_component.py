69b31d058369fd2047b28c3e02c225d92728a15c
jbellister-slac <jesseb@slac.stanford.edu>
2022-05-26 16:17:13 -0700

    TST: Add test cases for additional types and their associated arrays

--- pydm/tests/data_plugins/test_p4p_plugin_component.py
+++ pydm/tests/data_plugins/test_p4p_plugin_component.py
@@ -1,5 +1,6 @@
 import functools
-from p4p.client.thread import Context
+import numpy as np
+import pytest
 from p4p.nt import NTScalar
 from pydm.data_plugins.epics_plugins.p4p_plugin_component import Connection, P4PPlugin
 from pydm.tests.conftest import ConnectionSignals
@@ -13,7 +14,30 @@ class MockContext:
         self.monitor = None
 
 
-def test_send_new_value(monkeypatch: MonkeyPatch, signals: ConnectionSignals):
+def generate_control_variables(value):
+    """ Generate some set values for control variables to test against """
+    return {'value': value,
+            'valueAlarm': {'lowAlarmLimit': 2, 'lowWarningLimit': 3, 'highAlarmLimit': 10, 'highWarningLimit': 8},
+            'alarm': {'severity': 0},
+            'display': {'units': 'mV'},
+            'control': {'limitLow': 1, 'limitHigh': 11}
+            }
+
+
+@pytest.mark.parametrize('value_to_send, has_ctrl_vars, expected_value_to_receive', [
+    (NTScalar("i", display=True, control=True, valueAlarm=True).wrap(generate_control_variables(5)), True, 5),
+    (NTScalar("b", display=True, control=True, valueAlarm=True).wrap(generate_control_variables(1)), True, 1),
+    (NTScalar("h", display=True, control=True, valueAlarm=True).wrap(generate_control_variables(2)), True, 2),
+    (NTScalar("f", display=True, control=True, valueAlarm=True).wrap(generate_control_variables(7.0)), True, 7.0),
+    (NTScalar("s").wrap({'value': 'PyDM:TEST'}), False, 'PyDM:TEST'),
+    (NTScalar("ai").wrap({'value': [1, 2, 4]}), False, [1, 2, 4]),
+    (NTScalar("ab").wrap({'value': [6, 7, 8]}), False, [6, 7, 8]),
+    (NTScalar("ah").wrap({'value': [10, 11, 12]}), False, [10, 11, 12]),
+    (NTScalar("af").wrap({'value': [5.0, 6.0, 7.0, 8.0]}), False, [5.0, 6.0, 7.0, 8.0]),
+    (NTScalar("as").wrap({'value': ['PyDM', 'PVA', 'Test', 'Strings']}), False, ['PyDM', 'PVA', 'Test', 'Strings'])
+])
+def test_send_new_value(monkeypatch: MonkeyPatch, signals: ConnectionSignals,
+                        value_to_send: object, has_ctrl_vars: bool, expected_value_to_receive: object):
     """ Ensure that all our signals are emitted as expected based on the structured data we received from p4p """
 
     # Set up a mock p4p client
@@ -31,17 +55,10 @@ def test_send_new_value(monkeypatch: MonkeyPatch, signals: ConnectionSignals):
         nonlocal signals_received
         signals_received += 1
 
-    # Setup our initial testing value to send
-    nt = NTScalar("i", display=True, control=True, valueAlarm=True)
-    test_value = nt.wrap({'value': 5,
-                          'valueAlarm': {'lowAlarmLimit': 2, 'lowWarningLimit': 3,
-                                         'highAlarmLimit': 10, 'highWarningLimit': 8},
-                          'alarm': {'severity': 0},
-                          'display': {'units': 'mV'},
-                          'control': {'limitLow': 1, 'limitHigh': 11}
-                          })
-
-    p4p_connection.new_value_signal[int].connect(functools.partial(receive_signal, 'value'))
+    expected_value_type = type(value_to_send.value)
+    if type(value_to_send.value) == list:
+        expected_value_type = np.ndarray
+    p4p_connection.new_value_signal[expected_value_type].connect(functools.partial(receive_signal, 'value'))
     p4p_connection.lower_alarm_limit_signal.connect(functools.partial(receive_signal, 'low_alarm_limit'))
     p4p_connection.lower_warning_limit_signal.connect(functools.partial(receive_signal, 'low_warning_limit'))
     p4p_connection.upper_alarm_limit_signal.connect(functools.partial(receive_signal, 'high_alarm_limit'))
@@ -52,26 +69,30 @@ def test_send_new_value(monkeypatch: MonkeyPatch, signals: ConnectionSignals):
     p4p_connection.upper_ctrl_limit_signal.connect(functools.partial(receive_signal, 'upper_ctrl_limit'))
 
     # Send out the initial value for our test PV
-    p4p_connection.send_new_value(test_value)
+    p4p_connection.send_new_value(value_to_send)
 
     # Confirm all the signals were fired off as expected, and that each value is what we specified
-    assert received_values['value'] == 5
-    assert received_values['low_alarm_limit'] == 2
-    assert received_values['low_warning_limit'] == 3
-    assert received_values['high_warning_limit'] == 8
-    assert received_values['high_alarm_limit'] == 10
-    assert received_values['severity'] == 0
-    assert received_values['units'] == 'mV'
-    assert received_values['lower_ctrl_limit'] == 1
-    assert received_values['upper_ctrl_limit'] == 11
-    assert signals_received == 9
+    assert np.array_equal(received_values['value'], expected_value_to_receive)
+    if has_ctrl_vars:
+        assert received_values['low_alarm_limit'] == 2
+        assert received_values['low_warning_limit'] == 3
+        assert received_values['high_warning_limit'] == 8
+        assert received_values['high_alarm_limit'] == 10
+        assert received_values['severity'] == 0
+        assert received_values['units'] == 'mV'
+        assert received_values['lower_ctrl_limit'] == 1
+        assert received_values['upper_ctrl_limit'] == 11
+        assert signals_received == 9
+    else:
+        assert signals_received == 1
 
     # Now make just a couple changes to our value, and send it again
-    test_value.value = 9
-    test_value.alarm.severity = 1
-    p4p_connection.send_new_value(test_value)
+    if has_ctrl_vars:
+        value_to_send.value = 9
+        value_to_send.alarm.severity = 1
+        p4p_connection.send_new_value(value_to_send)
 
-    # Verify that the two values did update, and that the other 7 signals did not send
-    assert received_values['value'] == 9
-    assert received_values['severity'] == 1
-    assert signals_received == 11
+        # Verify that the two values did update, and that the other 7 signals did not send
+        assert received_values['value'] == 9
+        assert received_values['severity'] == 1
+        assert signals_received == 11
