cf5a16f5be4d61b4a67e9bb5fa6610d82a09ee5b
jbellister-slac <jesseb@slac.stanford.edu>
2022-05-27 11:46:38 -0700

    TST: Don't run new psp test while development dependency issues are worked out

--- run_tests.py
+++ run_tests.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 
     # Exclude p4p and pyca tests on Windows until p4p/pyepics compatibility issue is resolved and a Windows PyCA build exists
     if os.name == 'nt':
-        args.extend(['--ignore=pydm/tests/data_plugins/test_p4p_plugin_component.py',
-                     '--ignore=pydm/tests/data_plugins/test_psp_plugin_component.py'])
+        args.append('--ignore=pydm/tests/data_plugins/test_p4p_plugin_component.py')
+    args.append('--ignore=pydm/tests/data_plugins/test_psp_plugin_component.py')
 
     print('pytest arguments: {}'.format(args))
 
