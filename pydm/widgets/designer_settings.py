819d9754f13a867fa3e66e45be190ad495fdd580
jbellister-slac <jesseb@slac.stanford.edu>
2022-11-09 14:28:08 -0800

    FIX: Add PyDM to regex so it will parse as a single word

--- pydm/widgets/designer_settings.py
+++ pydm/widgets/designer_settings.py
@@ -287,7 +287,7 @@ def get_qt_properties(cls):
 
 
 def get_helper_label_text(attr: str) -> str:
-    spaced = re.sub("(.)([A-Z])", r"\1 \2", attr)
+    spaced = re.sub("(PyDM|.)([A-Z])", r"\1 \2", attr)
     return spaced.strip().capitalize()
 
 
