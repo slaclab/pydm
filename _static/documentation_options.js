679e98fd0d09c17cebbaca2609f67eac44521f3d
YektaY <YektaY@users.noreply.github.com>
2023-07-10 20:30:11 +0000

    deploy: c0d70ed28c7df694c329d705a284a2b938eeeaa4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g9ad9cd8',
+    VERSION: '0+untagged.1.gc0d70ed',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
