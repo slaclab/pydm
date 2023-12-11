4b6ceac15cfa90478aa2bb7dc935d3e64f76c7fd
Nolan Stelter <nstelter@slac.stanford.edu>
2023-12-11 05:21:37 -0800

    STY: Add comment on why using daemon threads for RPC polling

--- pydm/data_plugins/epics_plugins/p4p_plugin_component.py
+++ pydm/data_plugins/epics_plugins/p4p_plugin_component.py
@@ -415,6 +415,8 @@ class Connection(PyDMConnection):
                 self.connection_state_signal.emit(True)
                 self.emit_for_type(result.value)
                 if self._rpc_poll_rate > 0:
+                    # Use daemon threads so they will be stopped when all the non-daemon
+                    # threads (in our case just the main thread) are killed, preventing them from running forever.
                     self._background_polling_thread = threading.Thread(target=self.poll_rpc_channel, daemon=True)
                     # Start the thread
                     self._background_polling_thread.start()
