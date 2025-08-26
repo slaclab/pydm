1b0403b2dfff59bde84437d926040c5c023c7b06
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2025-08-26 15:38:46 -0300

    ENH: Update conda script and designer plugin file name for PySide6
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -8,20 +8,27 @@ mkdir -p $PREFIX/etc/pydm
 
 # Create auxiliary vars
 DESIGNER_PLUGIN_PATH=$PREFIX/etc/pydm
-DESIGNER_PLUGIN=$DESIGNER_PLUGIN_PATH/pydm_designer_plugin.py
+DESIGNER_PLUGIN=$DESIGNER_PLUGIN_PATH/register_pydm_designer_plugin.py
 ACTIVATE=$PREFIX/etc/conda/activate.d/pydm
 DEACTIVATE=$PREFIX/etc/conda/deactivate.d/pydm
 
-echo "from pydm.widgets.qtplugins import *" >> $DESIGNER_PLUGIN
+cat ./pydm/register_pydm_designer_plugin.py >> $DESIGNER_PLUGIN
 
 echo "export PYQTDESIGNERPATH=\$CONDA_PREFIX/etc/pydm:\$PYQTDESIGNERPATH" >> $ACTIVATE.sh
+echo "export PYSIDE_DESIGNER_PLUGINS=\$CONDA_PREFIX/etc/pydm:\$PYSIDE_DESIGNER_PLUGINS" >> $ACTIVATE.sh
 echo "unset PYQTDESIGNERPATH" >> $DEACTIVATE.sh
+echo "unset PYSIDE_DESIGNER_PLUGINS" >> $DEACTIVATE.sh
 
 echo '@echo OFF' >> $ACTIVATE.bat
 echo 'IF "%PYQTDESIGNERPATH%" == "" (' >> $ACTIVATE.bat
 echo 'set PYQTDESIGNERPATH=%CONDA_PREFIX%\etc\pydm' >> $ACTIVATE.bat
 echo ')ELSE (' >> $ACTIVATE.bat
 echo 'set PYQTDESIGNERPATH=%CONDA_PREFIX%\etc\pydm;%PYQTDESIGNERPATH%' >> $ACTIVATE.bat
+echo ')' >> $ACTIVATE.bat
+echo 'IF "%PYSIDE_DESIGNER_PLUGINS%" == "" (' >> $ACTIVATE.bat
+echo 'set PYSIDE_DESIGNER_PLUGINS=%CONDA_PREFIX%\etc\pydm' >> $ACTIVATE.bat
+echo ')ELSE (' >> $ACTIVATE.bat
+echo 'set PYSIDE_DESIGNER_PLUGINS=%CONDA_PREFIX%\etc\pydm;%PYSIDE_DESIGNER_PLUGINS%' >> $ACTIVATE.bat
 echo ')' >> $ACTIVATE.bat
 
 unset DESIGNER_PLUGIN_PATH
