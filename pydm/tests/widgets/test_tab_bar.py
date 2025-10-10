0f351e915a7bb73a31cdaac351b1d41d3ac6b214
jbellister-slac <jesseb@slac.stanford.edu>
2025-10-09 18:17:16 -0700

    TST: Add test case for constructing a PyDMTabWidget and setting the alarm channel on it

---
+++ pydm/tests/widgets/test_tab_bar.py
@@ -0,0 +1,54 @@
+import pytest
+from pydm.widgets.tab_bar import PyDMTabWidget
+from qtpy.QtCore import QByteArray
+from qtpy.QtWidgets import QWidget
+
+
+def test_construct(qtbot):
+    """
+    Test that creating a PyDMTabWidget succeeds without errors
+
+    Parameters
+    ----------
+     qtbot : fixture
+        Window for widget testing
+    """
+    parent = QWidget()
+    qtbot.addWidget(parent)
+
+    pydm_tab_widget = PyDMTabWidget(parent=parent)
+    qtbot.addWidget(pydm_tab_widget)
+
+    parent.deleteLater()
+    pydm_tab_widget.deleteLater()
+
+
+@pytest.mark.parametrize(
+    "new_alarm_channel",
+    [
+        "ca://MTEST",
+        QByteArray(b"ca://MTEST"),
+        "",
+    ],
+)
+def test_set_current_alarm_channel(qtbot, monkeypatch, new_alarm_channel):
+    """
+    Test that setting the alarm channel with both a regular string and a QByteArray works as expected
+
+    Parameters
+    ----------
+     qtbot : fixture
+        Window for widget testing
+     monkeypatch : fixture
+        To override default behaviors
+    new_alarm_channel: str or bytes
+        Test value to set the alarm channel to
+    """
+    pydm_tab_widget = PyDMTabWidget()
+    qtbot.addWidget(pydm_tab_widget)
+
+    monkeypatch.setattr(pydm_tab_widget.tabBar(), "currentIndex", lambda: 0)
+
+    pydm_tab_widget.currentTabAlarmChannel = new_alarm_channel
+    assert pydm_tab_widget.tabBar().currentTabAlarmChannel == new_alarm_channel
+    pydm_tab_widget.deleteLater()
