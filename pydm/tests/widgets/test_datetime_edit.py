d23aa01f7785be2369038180abeaa8bf82675663
Zachary Lentz <zlentz@slac.stanford.edu>
2026-04-01 14:44:08 -0700

    TST: add a test that shows failures for int in timebase seconds

--- pydm/tests/widgets/test_datetime_edit.py
+++ pydm/tests/widgets/test_datetime_edit.py
@@ -9,11 +9,6 @@ from pydm.widgets.datetime import PyDMDateTimeEdit, TimeBase
 from qtpy.QtCore import QDateTime
 
 
-# --------------------
-# POSITIVE TEST CASES
-# --------------------
-
-
 @pytest.mark.parametrize(
     "init_channel",
     [
@@ -44,11 +39,15 @@ def test_construct(qtbot, init_channel):
     else:
         assert pydm_datetimeedit.channel is None
 
-    assert pydm_datetimeedit.blockPastDate == True
-    assert pydm_datetimeedit.relative == True
+    assert pydm_datetimeedit.blockPastDate
+    assert pydm_datetimeedit.relative
     assert pydm_datetimeedit.timeBase == TimeBase.Milliseconds
 
 
+@pytest.mark.xfailif(
+    platform.system() == "Windows",
+    reason="These tests will fail on Windows, we can't easily modify time",
+)
 @pytest.mark.parametrize(
     "value, expected_value",
     [
@@ -102,3 +101,84 @@ def test_value_changed(qtbot, signals, value, expected_value):
     signals.new_value_signal[type(value)].emit(value)
 
     assert pydm_datetimeedit.text() == expected_value
+
+
+@pytest.mark.xfail(
+    platform.system() == "Windows",
+    reason="These tests will fail on Windows, we can't easily modify time",
+)
+@pytest.mark.parametrize(
+    "value, time_base",
+    [
+        (0, TimeBase.Milliseconds),
+        (1000, TimeBase.Milliseconds),
+        (60000, TimeBase.Milliseconds),
+        (3600000, TimeBase.Milliseconds),
+        (18000000, TimeBase.Milliseconds),
+        (0.0, TimeBase.Milliseconds),
+        (1000.0, TimeBase.Milliseconds),
+        (60000.0, TimeBase.Milliseconds),
+        (3600000.0, TimeBase.Milliseconds),
+        (18000000.0, TimeBase.Milliseconds),
+        (0, TimeBase.Seconds),
+        (1000, TimeBase.Seconds),
+        (60000, TimeBase.Seconds),
+        (3600000, TimeBase.Seconds),
+        (18000000, TimeBase.Seconds),
+        (0.0, TimeBase.Seconds),
+        (1000.0, TimeBase.Seconds),
+        (60000.0, TimeBase.Seconds),
+        (3600000.0, TimeBase.Seconds),
+        (18000000.0, TimeBase.Seconds),
+    ],
+)
+def test_value_round_trip(qtbot, value, time_base):
+    """
+    Test that using the widget as a setpoint is self-consistent.
+
+    Parameters
+    ----------
+    qtbot : fixture
+        pytest-qt window for widget testing
+    value : int, float
+        The milliseconds from the epoch, whose type can vary
+        depending on what widget we're hooked up to
+    time_base : TimeBase
+        Whether we're storing the time in seconds or milliseconds
+    """
+    os.environ["TZ"] = "America/New_York"
+    time.tzset()
+
+    pydm_datetimeedit = PyDMDateTimeEdit()
+    pydm_datetimeedit.blockPastDate = False
+    pydm_datetimeedit.relative = False
+    pydm_datetimeedit.timeBase = time_base
+    qtbot.addWidget(pydm_datetimeedit)
+
+    last_value = None
+
+    def collect_value(new_value):
+        nonlocal last_value
+        last_value = new_value
+
+    goal_type = type(value)
+
+    pydm_datetimeedit.channeltype = goal_type
+    pydm_datetimeedit.send_value_signal[goal_type].connect(collect_value)
+
+    dt = QDateTime()
+    if time_base == TimeBase.Milliseconds:
+        set_value = value
+    elif time_base == TimeBase.Seconds:
+        set_value = goal_type(value * 1000)
+    else:
+        raise RuntimeError(f"Please add new test branch for {time_base}")
+
+    dt.setMSecsSinceEpoch(int(set_value))
+
+    pydm_datetimeedit.setDateTime(dt)
+
+    with qtbot.waitSignal(pydm_datetimeedit.send_value_signal[goal_type], timeout=1000):
+        pydm_datetimeedit.send_value()
+
+    assert last_value == value
