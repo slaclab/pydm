e6dadc52880910210c7cceac1824df831e48e709
Yekta Yazar <yektay1@gmail.com>
2026-04-27 11:01:34 -0700

    Add title property to PyDMAnalogIndicator

--- pydm/tests/widgets/test_analog_indicator.py
+++ pydm/tests/widgets/test_analog_indicator.py
@@ -37,3 +37,62 @@ def test_construct(qtbot, init_channel):
     # ("Internal C++ object (PyDMDateTimeLabel) already deleted")
     parent.deleteLater()
     pydm_analog_indicator.deleteLater()
+
+
+def test_title_default(qtbot):
+    parent = QWidget()
+    qtbot.addWidget(parent)
+    indicator = PyDMAnalogIndicator(parent=parent)
+    qtbot.addWidget(indicator)
+
+    assert indicator.title == ""
+    assert indicator.title_label.isHidden()
+
+    parent.deleteLater()
+    indicator.deleteLater()
+
+
+def test_title_set_and_get(qtbot):
+    parent = QWidget()
+    qtbot.addWidget(parent)
+    indicator = PyDMAnalogIndicator(parent=parent)
+    qtbot.addWidget(indicator)
+
+    indicator.title = "Test Title"
+    assert indicator.title == "Test Title"
+    assert indicator.title_label.text() == "Test Title"
+    assert not indicator.title_label.isHidden()
+
+    parent.deleteLater()
+    indicator.deleteLater()
+
+
+def test_title_clear_hides_label(qtbot):
+    parent = QWidget()
+    qtbot.addWidget(parent)
+    indicator = PyDMAnalogIndicator(parent=parent)
+    qtbot.addWidget(indicator)
+
+    indicator.title = "Visible"
+    assert not indicator.title_label.isHidden()
+
+    indicator.title = ""
+    assert indicator.title_label.isHidden()
+
+    parent.deleteLater()
+    indicator.deleteLater()
+
+
+def test_title_reset(qtbot):
+    parent = QWidget()
+    qtbot.addWidget(parent)
+    indicator = PyDMAnalogIndicator(parent=parent)
+    qtbot.addWidget(indicator)
+
+    indicator.title = "Something"
+    indicator.resetTitle()
+    assert indicator.title == ""
+    assert indicator.title_label.isHidden()
+
+    parent.deleteLater()
+    indicator.deleteLater()
